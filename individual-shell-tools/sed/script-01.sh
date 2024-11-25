d827a5eb0d8881bfc89411c936773ecff66c6409
Daniel Wagner-Hall <dawagner@gmail.com>
2024-11-25 11:45:28 +0000

    Sample solutions for the initial shell tools exercises

--- individual-shell-tools/sed/script-01.sh
+++ individual-shell-tools/sed/script-01.sh
@@ -5,3 +5,4 @@ set -euo pipefail
 # TODO: Write a command to output input.txt with all occurrences of the letter `s` replaced with `S`.
 # The output should contain 11 lines.
 # The first line of the output should be: "ThiS iS a Sample file for experimenting with Sed.".
+sed -e 's/s/S/g' input.txt
