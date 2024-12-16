43dad89e64190e78cd13a545223af5021ee68da0
Daniel Wagner-Hall <dawagner@gmail.com>
2024-12-16 17:49:39 +0000

    Update individual-shell-tools/sed/script-01.sh
    Co-authored-by: Alasdair Smith <alasdairsmith100@gmail.com>

--- individual-shell-tools/sed/script-01.sh
+++ individual-shell-tools/sed/script-01.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Write a command to output input.txt with all occurences of the letter `s` replaced with `S`.
+# TODO: Write a command to output input.txt with all occurrences of the letter `s` replaced with `S`.
 # The output should contain 11 lines.
 # The first line of the outupt should be: "ThiS iS a Sample file for experimenting with Sed.".
