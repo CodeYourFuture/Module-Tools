d39074e7842dbee292b9bb0cc033e41b865d8836
Daniel Wagner-Hall <dawagner@gmail.com>
2024-12-30 16:02:45 +0000

    Swap jq 2 and 3

--- jq/script-02.sh
+++ jq/script-02.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 # The input for this script is the person.json file.
-# TODO: Write a command to output the name of the person, then a comma, then their profession.
-# Your output should be exactly the string "Selma, Software Engineer", but should not contain any quote characters.
+# TODO: Write a command to output the address of the person, all on one line, with a comma between each line.
+# Your output should be exactly the string "35 Fashion Street, London, E1 6PX", but should not contain any quote characters.
