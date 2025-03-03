d823cbe74e625cb5a3b6069304dea1284dad4182
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-03 15:10:59 +0000

    Enhance ls command to accept multiple paths as arguments

--- implement-shell-tools/ls/implementLs.js
+++ implement-shell-tools/ls/implementLs.js
@@ -3,23 +3,24 @@ import { promises as fs } from "node:fs";
 import { program } from "commander";
 
 program
-    .name("Implement ls")
-    .description("Implements a version of the ls command")
-    .option("-1, --one", "Output all files and directories each in a line")
-    .parse(process.argv);
+  .name("Implement ls")
+  .description("Implements a version of the ls command")
+  .option("-1, --one", "Output all files and directories each in a line")
+  .argument("[paths...]", "the paths")
+  .parse(process.argv);
 
-const filePaths = program.args.length ? program.args : ['.'];
+const filePaths = program.args.length ? program.args : ["."];
 const one = program.opts().one;
 
 filePaths.forEach(async (filePath) => {
-    try {
-        const files = await fs.readdir(filePath);
-        if (one) {
-            files.forEach(file => console.log(file));
-        } else {
-            console.log(files.join(' '));
-        }
-    } catch (err) {
-        console.error(`Error reading directory ${filePath}:`, err.message);
+  try {
+    const files = await fs.readdir(filePath);
+    if (one) {
+      files.forEach((file) => console.log(file));
+    } else {
+      console.log(files.join(" "));
     }
+  } catch (err) {
+    console.error(`Error reading directory ${filePath}:`, err.message);
+  }
 });
