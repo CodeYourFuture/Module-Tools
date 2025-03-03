cfa60fed0886a41187e2c871f1062de87952a5f8
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-03 12:07:39 +0000

    Refactor cat implementation to support reading multiple files asynchronously

--- implement-shell-tools/cat/implementCat.js
+++ implement-shell-tools/cat/implementCat.js
@@ -1,13 +1,20 @@
-import { promises as fs } from "node:fs";
+import { promises as fs, readFile } from "node:fs";
 import process from "node:process";
 
 const args = process.argv.slice(2);
 
-if (args.length != 1) {
-  console.error(`Expect one argument to be passed but got ${args.length} `);
-  process.exit(1);
+// if (args.length != 1) {
+//   console.error(`Expect one argument to be passed but got ${args.length} `);
+//   process.exit(1);
+// }
+
+async function readFiles(paths) {
+  try {
+    const promises = paths.map((filePath) => fs.readFile(filePath, "utf-8"));
+    const contents = await Promise.all(promises);
+    return contents;
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
-const path = args[0];
-const content = await fs.readFile(path, "utf-8");
-console.log(content);
