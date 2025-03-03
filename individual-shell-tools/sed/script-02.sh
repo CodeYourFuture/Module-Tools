55bc0ea39aa0753b2a8b001521c5740e6c1d8953
donarbl <donarablanc@gmail.com>
2025-03-03 17:16:38 +0000

    numbers removed using sed

--- individual-shell-tools/sed/script-02.sh
+++ individual-shell-tools/sed/script-02.sh
@@ -5,3 +5,17 @@ set -euo pipefail
 # TODO: Write a command to output input.txt with numbers removed.
 # The output should contain 11 lines.
 # Line 6 of the output should be " Alisha".
+
+sed 's/[0-9]*//g' input.txt # however Alisha is not on the 6 line 
+
+This is a sample file for experimenting with sed.
+
+It contains many lines, and there are some things you may want to do with each of them.       
+
+We'll include some score information:
+ Alisha
+ Jacob
+ Pietro
+ Katya
+
+We also should remember, when we go shopping, to get  items: oranges,cheese,bread,olives.
