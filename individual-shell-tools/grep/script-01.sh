c94f2276a21e20878c34d2d6efa95b6f4726bdc6
donarbl <donarablanc@gmail.com>
2025-03-03 16:36:11 +0000

    using carrot ^ to search doctor dialogue

--- individual-shell-tools/grep/script-01.sh
+++ individual-shell-tools/grep/script-01.sh
@@ -4,3 +4,12 @@ set -euo pipefail
 
 # TODO: Write a command to output every line in dialogue.txt said by the Doctor.
 # The output should contain 6 lines.
+
+grep ^Doctor dialogue.txt # i tried to use the "" quotes but my terminal didnt accept that
+
+Doctor: Hello
+Doctor: What's wrong today?
+Doctor: That sounds frustrating. When did this start?
+Doctor: Say "Hi".
+Doctor: You didn't say hello
+Doctor: You're welcome, goodbye
