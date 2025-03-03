215090560e2d1bccd6afb7bbc2a9451977717c72
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-03 14:14:02 +0000

    Add option to number non-empty lines in cat implementation

--- implement-shell-tools/cat/implementCat.js
+++ implement-shell-tools/cat/implementCat.js
@@ -6,6 +6,7 @@ program
   .name("Implement cat")
   .description("Implement a version of the cat program")
   .option("-n, --number", "Number the output lines, starting at 1")
+  .option("-b, --number2", "Number the nonempty lines, starting at 1")
   .argument("<paths...>", "The file paths to process")
   .parse(process.argv);
 
@@ -24,21 +25,29 @@ async function readFiles(paths) {
 }
 
 function extractLinesFromContent(content) {
-    const lines = content.split("\n");
-    if (lines[lines.length - 1] === "") {
-        lines.pop(); // excludes last line if it empty
-    }
-    return lines;
+  const lines = content.split("\n");
+  if (lines[lines.length - 1] === "") {
+    lines.pop(); // excludes last line if it empty
+  }
+  return lines;
 }
 
 async function displayFileContents() {
   const contents = await readFiles(filePaths);
   const flatternedContents = contents.flat();
   const numberLines = program.opts().number;
+  const numberLines2 = program.opts().number2;
+  let lineNumber = 1;
 
-  flatternedContents.forEach((line, index) => {
+  flatternedContents.forEach((line) => {
     if (numberLines) {
-      console.log(`${index + 1} ${line}`);
+      console.log(`${lineNumber++} ${line}`);
+    } else if (numberLines2) {
+      if (line !== "") {
+        console.log(`${lineNumber++} ${line}`);
+      } else {
+        console.log(line);
+      }
     } else {
       console.log(line);
     }
