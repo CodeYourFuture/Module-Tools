91e1f2aabfae98b795af4527768dad7fecf7a18e
donarbl <donarablanc@gmail.com>
2025-03-03 16:10:12 +0000

    counting words via wc -c

--- individual-shell-tools/cat/script-04-stretch.sh
+++ individual-shell-tools/cat/script-04-stretch.sh
@@ -17,4 +17,4 @@ set -euo pipefail
 # i didn't get this phrase "we want line numbers not to reset at the start of each file."
 # so there are two ways to do it
 cat -n ../helper-files/*txt # -n number the blank lines and filled out lines
-cat ../helper-files/*.txt |nl # skips the blank lines and comes only filled out lines
+cat ../helper-files/*.txt | nl # skips the blank lines and comes only filled out lines
