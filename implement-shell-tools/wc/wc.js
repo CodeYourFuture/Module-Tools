bad7699c29b09b354da2d5698d5dc5df0ade08b0
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-04 09:37:05 +0000

    Refactor wc command to handle multiple files and return line, word, and character counts for each

--- implement-shell-tools/wc/wc.js
+++ implement-shell-tools/wc/wc.js
@@ -4,15 +4,23 @@ import { promises as fs } from "node:fs";
 
 const args = process.argv.slice(2);
 
-const path = args[0];
-const content = await fs.readFile(path, { encoding: "utf-8" });
-const arrForLineCount = content.split("\n");
-const lineCount = arrForLineCount.length - 1;
-const arrForWordsCount = arrForLineCount.flatMap((element) =>
-  element.split(" ")
-);
-const charArray = arrForLineCount.flatMap((element) => element.split(""));
-const wordsCount = arrForWordsCount.length - 1;
-const charCount = charArray.length + 1;
+async function createLineWordsCharCountForFile(path) {
+  const content = await fs.readFile(path, { encoding: "utf-8" });
+  const arrForLineCount = content.split("\n");
+  const lineCount = arrForLineCount.length - 1;
+  const arrForWordsCount = arrForLineCount.flatMap((element) =>
+    element.split(" ")
+  );
+  const charArray = arrForLineCount.flatMap((element) => element.split(""));
+  const wordsCount = arrForWordsCount.length - 1;
+  const charCount = charArray.length + 1;
 
-console.log(`${lineCount} ${wordsCount} ${charCount} ${args[0]}`);
+  return `${lineCount} ${wordsCount} ${charCount} ${path}`;
+}
+
+async function createLineWordsCharCountForFiles() {
+  const files = await Promise.all(args.map(createLineWordsCharCountForFile));
+  files.forEach((file) => console.log(file));
+}
+
+createLineWordsCharCountForFiles();
