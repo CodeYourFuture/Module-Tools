3ffac880f36da9de8b6cf778378fbf454b2f6874
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-04 11:44:23 +0000

    Refactor wc command output logic to handle character option and display total count correctly

--- implement-shell-tools/wc/wc.js
+++ implement-shell-tools/wc/wc.js
@@ -66,11 +66,16 @@ async function createLineWordsCharCountForFiles() {
     return;
   }
 
-  //   aggregatedFilesData !== 0
-  //     ? console.log(
-  //         `${aggregatedFilesData[0]} ${aggregatedFilesData[1]} ${aggregatedFilesData[2]} total`
-  //       )
-  //     : null;
+  if (aggregatedFilesData !== 0 && charOption) {
+    console.log(`${aggregatedFilesData[0]} total`);
+    return;
+  }
+
+  aggregatedFilesData !== 0
+    ? console.log(
+        `${aggregatedFilesData[0]} ${aggregatedFilesData[1]} ${aggregatedFilesData[2]} total`
+      )
+    : null;
 }
 
 function aggregateFileData(files) {
