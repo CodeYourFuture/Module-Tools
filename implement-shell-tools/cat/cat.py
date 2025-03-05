59fb74ad16a849dd7bad3531008405c7abb5312c
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-05 20:32:55 +0000

    Refactor cat tool to return file content and print it in a loop

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -5,7 +5,9 @@ args = sys.argv[1:]
 def read_and_print_file_content(file_path):
     with open(file_path, 'r') as f:
         content = f.read()
-    print(content)
-
+    return content
 contents = map(read_and_print_file_content, args)
-print(list(contents))
+
+for content in contents:
+    if content is not None:
+        print(content)
