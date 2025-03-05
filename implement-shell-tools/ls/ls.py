f8f1ca8b818c42cbea93a5240029adb4e4719e5d
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-05 16:02:33 +0000

    Refactor ls tool to encapsulate file reading and printing in a function

--- implement-shell-tools/ls/ls.py
+++ implement-shell-tools/ls/ls.py
@@ -3,6 +3,10 @@ import sys
 
 args = sys.argv[1:]
 
-with open(args[0], 'r') as f: 
-    content = f.read()
-print(content)
+
+def read_and_print_file_content():
+    with open(args[0], 'r') as f: 
+        content = f.read()
+    print(content)
+
+read_and_print_file_content()
