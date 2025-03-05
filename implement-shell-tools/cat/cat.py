4fa86d0ab99d11a97b4b9b0ef52ecd4ca2dadb6b
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-05 21:12:23 +0000

    Refactor cat tool to use argparse for command-line argument parsing

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -1,12 +1,21 @@
-import sys
+import argparse
 
-args = sys.argv[1:]
+parser = argparse.ArgumentParser(
+    prog="cat program implementation",
+    description="Implements the cat program",
+)
+
+parser.add_argument("-n", "--number", help="The number for the line")
+parser.add_argument("paths", nargs='+', help="The files to search")
+
+
+args = parser.parse_args()
 
 def read_and_print_file_content(file_path):
     with open(file_path, 'r') as f:
         content = f.read()
     return content
-contents = map(read_and_print_file_content, args)
+contents = map(read_and_print_file_content, args.paths)
 
 for content in contents:
     if content is not None:
