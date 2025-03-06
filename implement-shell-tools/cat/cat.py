aa0ed1dc259f6449d5218aaeaa3c9e10340beb3e
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-06 10:03:52 +0000

    add option n for numbering lines for only one file passed as command line argument

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -5,18 +5,34 @@ parser = argparse.ArgumentParser(
     description="Implements the cat program",
 )
 
-parser.add_argument("-n", "--number", help="The number for the line")
-parser.add_argument("paths", nargs='+', help="The files to search")
+parser.add_argument("-n", "--number", action="store_true", help="Number the output lines, starting at 1.")
+parser.add_argument("path", help="The files to search")
 
 
 args = parser.parse_args()
+number_lines = args.number
+print(args)
+print(number_lines)
 
-def read_and_print_file_content(file_path):
+
+def read_and_print_file_content(file_path, number_lines):
+    count = 0
     with open(file_path, 'r') as f:
         content = f.read()
-    return content
-contents = map(read_and_print_file_content, args.paths)
+    lines = content.split('\n')
+    for line in lines:
+        if number_lines:
+            count+=1
+            print(count, line)
+        else:
+            print(line)
+        
+        
 
-for content in contents:
-    if content is not None:
-        print(content)
+
+read_and_print_file_content(args.path, number_lines)
+# contents = map(read_and_print_file_content, args.paths)
+
+# for content in contents:
+#     if content is not None:
+#         print(content)
