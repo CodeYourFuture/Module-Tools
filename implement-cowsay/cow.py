49ce112bf2361d73ba46b4d67aab3e6050b70695
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-06 20:21:41 +0000

    Enhance cowsay implementation with argparse for dynamic animal selection and message input

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,7 +1,14 @@
-import cowsay
-import sys
+import cowsay # type: ignore
+import argparse
 
+parser = argparse.ArgumentParser(description="Make animals say things")
+parser.add_argument('--animal', choices=cowsay.char_names, help="The animal to be saying things", default="cow")
+parser.add_argument('message', nargs='+', help="The message to say")
 
-args = sys.argv[1:]
+args = parser.parse_args()
 
-cowsay.cow(' '.join(args))
+char_name = args.animal
+message = ' '.join(args.message)
+
+cowsay_func = getattr(cowsay, char_name)
+cowsay_func(message)
