b9086885b4abc342f05b60d1855d2c58b2e438dd
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-06 11:36:27 +0000

    Implement basic ls command with directory listing functionality

---
+++ implement-shell-tools/ls/ls.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser(
+    prog="The ls program",
+    description="Implements the ls program"
+)
+parser.add_argument('dir', nargs='?', type=str, help="Path to the directory to list", default='.')
+parser.add_argument("-1", "--one", action="store_true", help="list the directory files one per line")
+
+args = parser.parse_args()
+print(args)
+
+def read_dir():
+    dir = args.dir
+    contents = os.listdir(dir)
+    for content in contents:
+        print(content)
+
+read_dir()
