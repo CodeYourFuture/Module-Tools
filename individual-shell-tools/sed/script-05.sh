bce28bcf416da3dc32330519c7c036c145f0f1d4
nadika <nadikazavodovska@gmail.com>
2025-03-07 10:07:20 +0000

    Resolve script-05 sed task

--- individual-shell-tools/sed/script-05.sh
+++ individual-shell-tools/sed/script-05.sh
@@ -6,3 +6,8 @@ set -euo pipefail
 # If a line starts with a number and a space, make the line instead end with a space and the number.
 # So line 6 which currently reads "37 Alisha" should instead read "Alisha 37".
 # The output should contain 11 lines.
+
+sed 's/^\([0-9]\+\) \(.*\)$/\2 \1/' input.txt
+
+# \([0-9]\+\ - matches one or more digits.
+# \(.*\)$/\2 \1/ - matches the rest of the line (2 - part after digit, 1 - part with digit).
