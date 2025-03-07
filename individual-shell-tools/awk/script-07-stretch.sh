49aa375c8f0d5b7584820688b8ea9b60650e1ed3
nadika <nadikazavodovska@gmail.com>
2025-03-07 10:58:59 +0000

    Resolve script-07 awk task

--- individual-shell-tools/awk/script-07-stretch.sh
+++ individual-shell-tools/awk/script-07-stretch.sh
@@ -7,3 +7,7 @@ set -euo pipefail
 # TODO: Write a command to output just the names of each player along with the total of adding all of that player's scores.
 # Your output should contain 6 lines, each with one word and one number on it.
 # The first line should be "Ahmed 15". The second line should be "Basia 37"
+
+awk '{scoreSum = 0; for( i = 3; i <= NF; i++) scoreSum += $i; print $1, scoreSum}' scores-table.txt
+
+# count sum of the score using for loop.
