873c8a15dac4704e822bc1dc6037ec268023689c
nadika <nadikazavodovska@gmail.com>
2025-03-07 08:54:12 +0000

    Resolve script-01 grep task

--- individual-shell-tools/grep/script-01.sh
+++ individual-shell-tools/grep/script-01.sh
@@ -4,3 +4,8 @@ set -euo pipefail
 
 # TODO: Write a command to output every line in dialogue.txt said by the Doctor.
 # The output should contain 6 lines.
+
+grep "^Doctor" dialogue.txt
+
+# ^ - ensures "Doctor" word appears at the start of the line.
+
