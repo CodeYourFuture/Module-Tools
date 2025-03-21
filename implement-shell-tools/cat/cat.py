fa3122dea2652651eeb37e7ed4578e8c85752ec8
Anna Fedyna <annafedyna03@gmail.com>
2025-03-21 09:28:02 +0000

    added error handling for cat command

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -11,14 +11,19 @@ parser.add_argument("paths", nargs="+", help="The file to search")
 args = parser.parse_args()
 
 for path in args.paths:
-    with open(path, "r") as f:
-        content = f.read()
-    if args.n or args.b :
-        counter = 1
-        arr = content.split('\n')[:-1]
-        for line in arr:
-            if args.b and not line:
-                print(line)
-                continue
-            print(counter, line)
-            counter += 1
+    try:
+        with open(path, "r") as f:
+            content = f.read()
+        if args.n or args.b :
+            counter = 1
+            arr = content.split('\n')[:-1]
+            for line in arr:
+                if args.b and not line:
+                    print(line)
+                    continue
+                print(counter, line)
+                counter += 1
+    except FileNotFoundError:
+        print(f"Error: File '{path}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
