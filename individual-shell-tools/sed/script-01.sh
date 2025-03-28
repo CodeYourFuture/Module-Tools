874ebdddf0dc747ad7c6d27c95f8a25115bdc5ff
Pooriya Ketabi <159670309+PooriyaKTB@users.noreply.github.com>
2025-03-28 11:53:23 +0000

    some changes applied

--- individual-shell-tools/sed/script-01.sh
+++ individual-shell-tools/sed/script-01.sh
@@ -7,3 +7,5 @@ set -euo pipefail
 # The first line of the output should be: "ThIs Is a sample fIle for experImentIng with sed.".
 
 sed s/i/I/g ./input.txt
+
+#   **** FYI: In the line 7,it seems there is a mistake, as example for output looks like `with` is not replaced with `wIth`, but it should be replaced. ****
