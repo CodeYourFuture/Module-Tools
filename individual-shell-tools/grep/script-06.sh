45066a894f2c4cd8b47b4c7a815a70d93cc6780c
Luke-Manyamazi <lukemanyamazi1@gmail.com>
2025-07-13 21:01:26 +0200

    added code for grep exercise 6

--- individual-shell-tools/grep/script-06.sh
+++ individual-shell-tools/grep/script-06.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 
 # TODO: Write a command to output the name of every `.txt` file in this directory which contains a line of dialogue said by the Doctor.
 # The output should contain two filenames.
+grep -l "Doctor:" "$(dirname "$0")"/*.txt | while IFS= read -r filepath; do
+    basename "$filepath"
+done
