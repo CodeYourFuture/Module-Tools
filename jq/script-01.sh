e5d156d618b47f4e8a9bf1247c660c0cff343bd8
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2025-07-18 16:06:44 +0100

    jq solutions from 1-6 done

--- jq/script-01.sh
+++ jq/script-01.sh
@@ -5,3 +5,7 @@ set -euo pipefail
 # The input for this script is the person.json file.
 # TODO: Write a command to output the name of the person.
 # Your output should be exactly the string "Selma", but should not contain any quote characters.
+
+#ANSWER
+jq -r '.name' person.json
+#-r to remove the default '' jq adds tgo strings ,jq '.name' extracts the value of the name filed.
