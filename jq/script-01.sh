7320081cfa70d40ce33bb2d6cd5dd6f6ef15aca6
Luke-Manyamazi <lukemanyamazi1@gmail.com>
2025-07-22 18:14:39 +0200

    added code for jq script 4

--- jq/script-01.sh
+++ jq/script-01.sh
@@ -6,3 +6,4 @@ set -euo pipefail
 # TODO: Write a command to output the name of the person.
 # Your output should be exactly the string "Selma", but should not contain any quote characters.
 jq -r '.name' "/home/luke/CYF/SDC/Module-Tools/jq/person.json"
+ 
