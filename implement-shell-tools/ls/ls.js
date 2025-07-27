9907becf9196db405818ea17eb3aa7f3eb0fc1b4
Droid-An <andrei.filippov.aaf@gmail.com>
2025-07-27 08:45:30 +0100

    done ls

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -1,4 +1,4 @@
-import { promises as fs } from "node:fs";
+import { Dirent, promises as fs } from "node:fs";
 import process from "node:process";
 import path from "node:path";
 import { program } from "commander";
@@ -15,49 +15,104 @@ program
 
 program.parse();
 
-/* probably can be assigned by ternary operator */
 let pathToFile = "";
 
 const programArgv = program.args;
-if (programArgv.length == 1) {
-  pathToFile = programArgv[0];
-} else if (programArgv.length == 0) {
-  pathToFile = process.argv[1];
-} else {
-  console.error(
-    `Expected no more than 1 argument (sample-files) to be passed but got ${argv.length}.`
-  );
-}
+// if (programArgv.length == 1) {
+//   console.log(programArgv[0]);
+//   fs.stat(programArgv[0], (err, stats) => {
+//     console.log(stats);
+//     if (!err) {
+//       if (stats.isFile()) {
+//         console.log("is file ? " + stats.isFile());
+//         await listFiles();
+//       } else if (stats.isDirectory()) {
+//         console.log("is directory? " + stats.isDirectory());
+//         listFiles();
+//       }
+//     } else throw err;
+//   });
 
-const char = program.opts();
+//   pathToFile = programArgv[0];
+// } else if (programArgv.length == 0) {
+//   pathToFile = process.argv[1];
+//   listFiles();
+// } else {
+//   console.error(
+//     `Expected no more than 1 argument (sample-files) to be passed but got ${argv.length}.`
+//   );
+// }
 
-const formattedPath = path.dirname(pathToFile);
-
-async function listFiles() {
-  /* can be rewritten using switch */
-  if (char["1"]) {
+(async () => {
+  if (programArgv.length === 1) {
+    pathToFile = programArgv[0];
     try {
-      const files = await fs.readdir(formattedPath);
-      const sortedOutput = files.sort((a, b) => a.localeCompare(b));
-      files.forEach(function (file) {
-        console.log(file);
-      });
+      const stats = await fs.stat(pathToFile);
+      if (stats.isFile()) {
+        console.log("is file ? " + stats.isFile());
+        await listFiles("file");
+      } else if (stats.isDirectory()) {
+        console.log("is directory? " + stats.isDirectory());
+
+        listFiles("directory");
+      } else {
+        console.error("Not a file or directory.");
+      }
     } catch (err) {
-      //is it goes to stderror
-      console.error("Error reading directory:", err);
+      console.error("Invalid path:", err.message);
     }
-  } else if (char["a"]) {
-    try {
-      const files = await fs.readdir(formattedPath);
-      const formattedOutput = files.sort((a, b) => a.localeCompare(b));
-      formattedOutput.unshift("..");
-      formattedOutput.unshift(".");
-      console.log(formattedOutput.join("             "));
-    } catch (err) {
-      //is it goes to stderror
-      console.error("Error reading directory:", err);
+  } else if (programArgv.length === 0) {
+    pathToFile = process.cwd();
+    await listFiles("directory");
+  } else {
+    console.error(
+      `Expected no more than 1 argument (sample-files) but got ${programArgv.length}.`
+    );
+  }
+})();
+
+const flag_1 = (files) => {
+  try {
+    files.forEach(function (file) {
+      console.log(file);
+    });
+  } catch (err) {
+    //is it goes to stderror
+    console.error("Error reading directory:", err);
+  }
+};
+
+const flag_a = (files) => {
+  try {
+    files.unshift("..");
+    files.unshift(".");
+    return files;
+  } catch (err) {
+    //is it goes to stderror
+    console.error("Error reading directory:", err);
+  }
+};
+
+async function listFiles(type) {
+  try {
+    let formattedPath = "";
+    if (type == "directory") {
+      formattedPath = pathToFile;
+    } else if (type == "file") {
+      formattedPath = path.dirname(pathToFile);
     }
+    const char = program.opts();
+    const files = await fs.readdir(formattedPath);
+    const sortedOutput = files.sort((a, b) => a.localeCompare(b));
+
+    if (char["1"] && char["a"]) {
+      flag_1(flag_a(sortedOutput));
+    } else if (char["a"]) {
+      console.log(flag_a(sortedOutput).join("     "));
+    } else if (char["1"]) {
+      flag_1(sortedOutput);
+    }
+  } catch (err) {
+    console.error("Error reading directory:", err);
   }
 }
-
-listFiles();
