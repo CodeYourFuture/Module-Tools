82d13541178090dc81fd654e3dae4247bbda92c8
sheetalkharab <sheetalrana89@gmail.com>
2025-07-27 15:02:14 +0100

    update for -b flag

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -6,6 +6,7 @@ program
     .name("cat")
     .description("print the content of file")
     .option("-n, --line-numbers","Number the output lines, starting at 1")
+    .option("-b, --number-nonblank", "Number non-empty output lines, overrides -n")
     .argument("<paths...>", "The file path(s) to process"); // to support multiple file
 program.parse();
 
@@ -25,7 +26,18 @@ const lines= content.split(/\r?\n/);
 if (lines.length && lines[lines.length - 1] === '') {//// Remove trailing empty line if it's just from the final newline
     lines.pop();
 }
-if (options.lineNumbers) {
+
+if (options.numberNonblank) {
+            lines.forEach((line) => {
+                if (line.trim() === "") {
+                    console.log(""); // Blank line, no number
+                } else {
+                    const lineNumber = String(lineCounter++).padStart(6, " ");
+                    console.log(`${lineNumber}\t${line}`);
+                }
+            });
+        
+}else if (options.lineNumbers) {
     lines.forEach((line) => {
         const lineNumber = String(lineCounter++).padStart(6, ' ');
         console.log(`${lineNumber}\t${line}`)
