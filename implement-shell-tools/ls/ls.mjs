2c78b432c69eef7f80c6390cf341f8ac186878ad
Faithy4444 <faithy4444@gmail.com>
2025-07-28 20:04:27 +0200

    ls command with flags

---
+++ implement-shell-tools/ls/ls.mjs
@@ -0,0 +1,34 @@
+import {program} from "commander";
+import {promises as fs} from "node:fs";
+import { readdir } from "node:fs";
+
+program
+.name("ls")
+.description("displays files in a directory")
+.option("-1, --one", "displays each file in its own line")
+.option("-a, --all", "displays all files including hidden files")
+.arguments("<filepath>")
+
+await program.parseAsync();
+
+const opts = program.opts();
+const args = program.args;
+
+if (args.length === 0) {
+  console.error("Error: Missing <filepath> argument.");
+  program.help();
+}
+
+const path = args[0];
+const files = await fs.readdir(path, "utf-8");
+
+let visibleFiles = files;
+if (!opts.all) {
+  visibleFiles = files.filter(file => !file.startsWith('.'));
+}
+
+if (opts.one) {
+  visibleFiles.forEach(file => console.log(file));
+} else {
+  console.log(visibleFiles.join(' '));
+}
