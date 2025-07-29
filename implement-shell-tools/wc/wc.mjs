5679649ce3826b4a85afe983c4b72d0a5d3e8ea9
sheetalkharab <sheetalrana89@gmail.com>
2025-07-29 18:09:16 +0100

    update code to include -l flag

--- implement-shell-tools/wc/wc.mjs
+++ implement-shell-tools/wc/wc.mjs
@@ -4,10 +4,12 @@ import { promises as fs } from "node:fs";
 program
   .name("wc")
   .description("Count lines, words, and bytes in files")
+  .option("-l, --lines", "Only print line counts")
   .argument("<files...>", "One or more files to process");
 await program.parseAsync();
 
 const files = program.args;
+const options = program.opts();
 
 function countContent(content){
     const lines = (content.match(/\n/g) || []).length;
@@ -24,7 +26,12 @@ for(const file of files){
     try{
         const content = await fs.readFile(file, "utf-8");
         const {lines, words, bytes } = countContent(content);
-        console.log(`${lines} ${words} ${bytes} ${file}`);
+        
+        if (options.lines) {
+            console.log(`${lines} ${file}`);
+        } else {
+            console.log(`${lines} ${words} ${bytes} ${file}`);
+        }
 
         totalLines += lines;
         totalWords += words;
@@ -36,5 +43,5 @@ for(const file of files){
 }
 
 if (files.length > 1) {
-  console.log(`${totalLines} ${totalWords} ${totalBytes} total`);
+    console.log(`${totalLines} total`);
 }
