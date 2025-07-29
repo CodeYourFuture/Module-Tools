84aee2dd10124a7f5ad1a58029ed379f884fd899
Faithy4444 <faithy4444@gmail.com>
2025-07-29 11:17:58 +0200

    wc command complete

---
+++ implement-shell-tools/wc/wc.mjs
@@ -0,0 +1,51 @@
+import {program} from "commander";
+import{promises as fs} from "node:fs";
+
+program
+.name("wc")
+.description("counts lines, words, and bytes")
+.option("-l, --line", "counts number of lines in the file")
+.option("-w, --words", "counts number of words in the file")
+.option("-c, --bytes", "counts number  of bytes in the file")
+.argument("<filepath>")
+
+program.parse();
+
+const args= program.args;
+const opts = program.opts();
+
+if(args.length === 0){
+    console.error("Error: Missing <filepath> argument.");
+  program.help();
+}
+
+const path = args[0];
+const content = await fs.readFile(path, "utf-8")
+
+
+// function to count the number of lines in a files
+function countLines(content){
+const lines = content.split(/\r?\n/)
+return lines.length
+}
+
+//function to count the words
+function countWords(content){
+  const words = content.trim().split(/\s+/);
+  return words.length
+}
+
+//function to count the bytes
+function countBytes(content){
+  return Buffer.byteLength(content, "utf-8");
+}
+
+if (opts.line){
+  console.log(countLines(content))
+}else if(opts.words){console.log(countWords(content), path)}
+else if(opts.bytes){console.log(countBytes(content), path)}
+else{
+  console.log(countLines(content), countWords(content), countBytes(content), path)
+}
+
+
