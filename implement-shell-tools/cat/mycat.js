e2a0265d7668d50c1769104f10ee2da7e0ad8689
Priscilla <cpjm0033@gmail.com>
2025-07-30 06:21:17 +0100

    using the commander library

--- implement-shell-tools/cat/mycat.js
+++ implement-shell-tools/cat/mycat.js
@@ -1,14 +1,22 @@
-import process from "node:process";
+import { program } from "commander";
 import { promises as fs } from "node:fs";
+import process from "node:process";
 
-const argv = process.argv.slice(2);
-if (argv.length != 1) {
+program
+  .name("mycat")
+  .description("Outputs the content of the given file(s), like the cat command")
+  .argument("<path>", "The file path to read");
+
+program.parse();
+
+const argv = program.args;
+if (argv.length !== 1) {
   console.error(
     `Expected exactly 1 argument (a path) to be passed but got ${argv.length}.`
   );
   process.exit(1);
 }
+
 const path = argv[0];
- 
 const content = await fs.readFile(path, "utf-8");
 process.stdout.write(content);
