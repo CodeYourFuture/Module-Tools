deac01e418d6a6c7abc7ef5c6732047af83c9fbb
Fradoka <franklindkamela@gmail.com>
2025-07-31 14:39:22 +0100

    addressed all review feedbacks

--- individual-shell-tools/ls/script-03.sh
+++ individual-shell-tools/ls/script-03.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 # TODO: Write a command which _recursively_ lists all of the files and folders in this directory _and_ all of the files inside those folders.
 # The output should be a list of names including: child-directory, script-01.sh, helper-1.txt (and more).
 # The formatting of the output doesn't matter.
-find .
+# find .
+thisDir="/Users/cyf/Documents/SDC/Module-Tools/individual-shell-tools/ls"
+ls -R "$thisDir"
