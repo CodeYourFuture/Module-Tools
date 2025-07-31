033c4d13cbb6d9ca9d23497f547d0bef6e0a8073
aiah ibrahim <ayababikeribrahim@gmail.com>
2025-07-31 21:39:18 +0100

    cat1

---
+++ implement-shell-tools/cat/cat.js
@@ -0,0 +1,32 @@
+import { program } from "commander";
+import process from "node:process";
+import { promises as fs } from "node:fs";
+
+program
+  .name("cat")
+  .description("Prints file contents to the terminal")
+  .argument("<files...>", "Files to print");
+
+program.parse();
+
+const files = program.args;
+
+if (files.length < 1) {
+  console.error("Please specify one or more files.");
+  process.exit(1);
+}
+
+async function printFileContent(filename) {
+  try {
+    const content = await fs.readFile(filename, "utf-8");
+    process.stdout.write(content);
+  } catch {
+    console.error(`Could not read file: ${filename}`);
+  }
+}
+
+(async () => {
+  for (const file of files) {
+    await printFileContent(file);
+  }
+})();
