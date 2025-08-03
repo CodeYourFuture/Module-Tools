9914ae491731126a41d469a40f047fa3db9b1387
Droid-An <andrei.filippov.aaf@gmail.com>
2025-08-03 13:05:23 +0100

    done cow.py

---
+++ implement-cowsay/cow.py
@@ -0,0 +1,19 @@
+import cowsay
+import sys 
+import argparse
+import os
+
+list_of_choices = cowsay.char_names
+
+
+parser = argparse.ArgumentParser(
+    prog="cowsay",
+    description="Make animals say things",
+)
+
+parser.add_argument("--animal", nargs="?",default="cow",  help="The animal to be saying things.", choices=list_of_choices)
+parser.add_argument("message", nargs="*", default="", help="message to say")
+
+args = parser.parse_args()
+
+print(cowsay.get_output_string(str(args.animal)," ".join(args.message)))
