41888878153b5cb2e86bd6600197b5bae4701587
Gabriel <deng93003@gmail.com>
2025-08-05 00:37:31 +0100

    The functionality for the cowsay project in python.

--- implement-cowsay/cow_script.py
+++ implement-cowsay/cow_script.py
@@ -1,4 +1,23 @@
 import cowsay
-import sys
+import argparse
 
-cowsay.cow(" ".join(sys.argv[1:]))
+parser = argparse.ArgumentParser(
+    prog="Display items",
+    description="Display the contents of a file reminiscent of the cat command"
+)
+
+parser.add_argument("--animal", help="Gives the user the option of adding the animal")
+parser.add_argument("message", nargs="+", help="The message the animal needs to say.")
+
+args = parser.parse_args()
+
+animal_entered = args.animal
+message_to_say = " ".join(args.message)
+
+animals = [animal for animal in dir(cowsay) if callable(getattr(cowsay, animal))]
+
+if animal_entered in animals:
+    getattr(cowsay, animal_entered)(message_to_say)
+else:
+    print(f"'{animal_entered}' is not a valid animal. please choose from '{animals[1:]}'")
+    print(f"Try one of: {', '.join(animals)}")
