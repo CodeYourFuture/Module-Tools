bd0d84eb0f41131aa4421889f5d487505491f348
Fatma <fatma.arslantas@outlook.com>
2025-08-05 10:16:22 +0100

    Implemented try-except block

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -1,22 +1,28 @@
 import argparse
+import sys
 
 def cat_file(file_path, number_all_lines, number_non_blank):
-  with open(file_path, "r", encoding="utf-8") as file:
-        lines = file.readlines()
+    try:
+       with open(file_path, "r", encoding="utf-8") as file:
+            lines = file.readlines()
 
-        for line_num, line in enumerate(lines, start=1):
-            prefix = ""
+            for line_num, line in enumerate(lines, start=1):
+                prefix = ""
 
-            # -n: number all lines
-            if number_all_lines:
-                prefix = f"{line_num:6}\t" # 'line_num:6' means align right
+                # -n: number all lines
+                if number_all_lines:
+                    prefix = f"{line_num:6}\t" # 'line_num:6' means align right
 
-            # -b: number only non-blank lines
-            elif number_non_blank and line.strip() != "":
-                    prefix = f"{line_num:6}\t"
+                # -b: number only non-blank lines
+                elif number_non_blank and line.strip() != "":
+                        prefix = f"{line_num:6}\t"
 
-            print(f"{prefix}{line}", end="")
-            
+                print(f"{prefix}{line}", end="")
+    except FileNotFoundError:
+        print(f"cat: {file_path}: No such file or directory", file=sys.stderr)
+    except Exception as e:
+        print(f"cat: {file_path}: Error reading file: {e}", file=sys.stderr)
+
 
 parser = argparse.ArgumentParser(
     prog="cat.py",
