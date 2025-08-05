bc438f91b439db45adb44261c9ff0cea1fb2a302
Fatma <fatma.arslantas@outlook.com>
2025-08-05 11:29:35 +0100

    refactor: Apply ruff formatting

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -4,12 +4,14 @@ import cowsay
 # Get the list of animals from cowsay
 available_animals = cowsay.char_names
 
-parser = argparse.ArgumentParser(
-    prog="cow.py",
-    description="Make animals say things"
-)
+parser = argparse.ArgumentParser(prog="cow.py", description="Make animals say things")
 
-parser.add_argument("--animal", choices=available_animals, default="cow", help="The animal to be saying things.")
+parser.add_argument(
+    "--animal",
+    choices=available_animals,
+    default="cow",
+    help="The animal to be saying things.",
+)
 parser.add_argument("message", nargs="+", help="The message to say.")
 
 args = parser.parse_args()
