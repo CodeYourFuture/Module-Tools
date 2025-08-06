c7e390957c8a375d424c763060ccfd3885fb795c
sheetalkharab <sheetalrana89@gmail.com>
2025-08-06 22:24:40 +0100

    cat folder update code to implement -b flag

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -6,10 +6,17 @@ parser = argparse.ArgumentParser(description="Python implementation of cat comma
 
 # to number all lines
 parser.add_argument("-n", action="store_true", help="Number all lines")
+
+# to number non-blank lines
+parser.add_argument("-b", action="store_true", help="Number non-blank lines (overrides -n)")
+
 # To raed files
 parser.add_argument("files", nargs="+", help="Files to read")
 
 args = parser.parse_args()
+
+if args.n and args.b:
+    args.n = False
 
 line_number = 1
 
@@ -21,7 +28,14 @@ for file in args.files:
 
     with open(file, "r") as f:
         for line in f:
-            if args.n:
+            if args.b:
+                if line.strip():      #to number non blank lines only
+                    print(f"{line_number:6}\t{line}", end="")
+                    line_number += 1
+                else:
+                    print(line, end="") # to print blank line with number
+                       
+            elif args.n:
                 print(f"{line_number:6}\t{line}", end="")  # to print number all lines
                 line_number += 1
             else:    
