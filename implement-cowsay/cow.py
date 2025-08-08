28a3a0a2837aca22a3c67a16cb46dede0ba1390f
Fradoka <franklindkamela@gmail.com>
2025-08-08 13:52:47 +0100

    completed cowsay

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,27 +1,25 @@
 import argparse
+import cowsay
 import sys
-from cowpy import cow
+
+available_animals = cowsay.char_names
 
 parser = argparse.ArgumentParser(
     prog="cowsay",
-    description="uses the cowsay library to show user-supplied text, as said by a specified animal",
+    description="Uses the cowsay library to show user-supplied text, as said by a specified animal",
 )
 
-parser.add_argument("--animal", help="The animal to be saying things", default="cowacter")
+parser.add_argument("--animal", help="The animal to be saying things", default="cow", choices=available_animals)
 parser.add_argument("message", help="The message to say", nargs="*", default=["Hello, World!"])
 
 args = parser.parse_args()
 
-
-available_animals = {name.lower(): name for name in cow.Cowacter.cowacters} # List of all characters
-
 msg = " ".join(args.message)
 animal = args.animal.lower()
 
-if animal not in available_animals:
-    print(f"Invalid choice: '{animal}' (choose from {', '.join(available_animals)})")
+if not hasattr(cowsay, animal):
+    print(f"Invalid choice: '{args.animal}' (choose from: {', '.join(available_animals)})")
     sys.exit(1)
 
-animal_class_name = available_animals[animal]
-AnimalClass = getattr(cow, animal_class_name)
-print(AnimalClass().milk(msg))
+# Dynamically call the cowsay function for the chosen animal
+getattr(cowsay, animal)(msg)
