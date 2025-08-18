b90ef4108129264721a4d9b76e118ea7b69d1695
Faithy4444 <faithy4444@gmail.com>
2025-08-18 20:26:56 +0200

    validation using argparse

--- implement-cowsay/main.py
+++ implement-cowsay/main.py
@@ -8,13 +8,5 @@ parser.add_argument('animal', type=str, help= 'please enter a valid animal')
 parser.add_argument('message', type=str, help= 'please enter a message')
 arg = parser.parse_args()
 
-valid_animals = cowsay.char_names
-
-if arg.animal in valid_animals:
-     animal_func = getattr(cowsay, arg.animal)
-     print(animal_func(arg.message))
-    
-else:
-    print(f"Error: '{arg.animal}' is not a valid animal.")
-    print("Valid animals are:", ", ".join(valid_animals))
-    sys.exit(1)
+animal_func = getattr(cowsay, arg.animal)
+print(animal_func(arg.message))
