28ac4c30dbca24c4effd45872091490181c22605
Faithy4444 <faithy4444@gmail.com>
2025-08-19 09:59:44 +0200

    validation fixed

--- implement-cowsay/main.py
+++ implement-cowsay/main.py
@@ -4,7 +4,7 @@ import sys
 
 
 parser = argparse.ArgumentParser(description="displays cowsay charactor with given arguments")
-parser.add_argument('animal', type=str, help= 'please enter a valid animal')
+parser.add_argument('animal', type=str, choices=cowsay.char_names, default="cow", help= 'please enter a valid animal')
 parser.add_argument('message', type=str, help= 'please enter a message')
 arg = parser.parse_args()
 
