4ffbb6770b5f42a917dfc5707a7008e6b518308a
Luke Manyamazi <lukemanyamazi1@gmail.com>
2025-10-14 21:57:05 +0200

    Update script-03.sh

--- individual-shell-tools/wc/script-03.sh
+++ individual-shell-tools/wc/script-03.sh
@@ -8,4 +8,4 @@ set -euo pipefail
 #        1       7      39 ../helper-files/helper-2.txt
 #        3      19      92 ../helper-files/helper-3.txt
 #        5      30     151 total
-wc -lwm "$(dirname "$0")"/../helper-files/*.txt
+wc "$(dirname "$0")"/../helper-files/*.txt
