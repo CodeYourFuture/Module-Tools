f7ae788d7966c99d5f01055be26e80d64c2ca940
Gabriel <deng93003@gmail.com>
2025-10-25 21:01:59 +0100

    These are the programs for the cow-say project

--- implement-cowsay/cow_script.py
+++ implement-cowsay/cow_script.py
@@ -1,23 +1,29 @@
 import cowsay
 import argparse
 
+animals = [a for a in dir(cowsay) if callable(getattr(cowsay, a)) and not a.startswith("_")]
+
 parser = argparse.ArgumentParser(
-    prog="Display items",
-    description="Display the contents of a file reminiscent of the cat command"
+    prog="cowsay",
+    description="Make animals say things"
 )
 
-parser.add_argument("--animal", help="Gives the user the option of adding the animal")
-parser.add_argument("message", nargs="+", help="The message the animal needs to say.")
+parser.add_argument(
+    "--animal",
+    choices=animals,
+    help="The animal to be saying things."
+)
+
+parser.add_argument(
+    "message",
+    nargs="+",
+    help="The message to say."
+)
 
 args = parser.parse_args()
-
-animal_entered = args.animal
 message_to_say = " ".join(args.message)
 
-animals = [animal for animal in dir(cowsay) if callable(getattr(cowsay, animal))]
-
-if animal_entered in animals:
-    getattr(cowsay, animal_entered)(message_to_say)
+if args.animal:
+    getattr(cowsay, args.animal)(message_to_say)
 else:
-    print(f"'{animal_entered}' is not a valid animal. please choose from '{animals[1:]}'")
-    print(f"Try one of: {', '.join(animals)}")
+    cowsay.cow(message_to_say)
