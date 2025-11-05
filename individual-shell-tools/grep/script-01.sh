5f214aa768f4ae8a352451d373a708fab842425e
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-11-05 13:43:09 +0200

    grep exercises complete

--- individual-shell-tools/grep/script-01.sh
+++ individual-shell-tools/grep/script-01.sh
@@ -3,4 +3,6 @@
 set -euo pipefail
 
 # TODO: Write a command to output every line in dialogue.txt said by the Doctor.
+grep "^Doctor\b" dialogue.txt
+
 # The output should contain 6 lines.
--- individual-shell-tools/grep/script-04.sh
+++ individual-shell-tools/grep/script-04.sh
@@ -3,4 +3,6 @@
 set -euo pipefail
 
 # TODO: Write a command to output every line in dialogue.txt that does not contain the word "Hello" (regardless of case).
+grep -icv Hello dialogue.txt
+
 # The output should contain 10 lines.
