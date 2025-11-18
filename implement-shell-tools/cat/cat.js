b640defd0bab31c7af6125e3a0da127f3c1e09d7
sheida-shab <shabankary@gmail.com>
2025-11-18 23:46:18 +0000

    implementing wild cards pattern

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -1,7 +1,13 @@
 import { promises as fs } from "node:fs";
+import { glob } from "glob";
  async function readFile(){
-    const fileName=process.argv[2];
-    const fileContent=await fs.readFile(fileName , 'utf-8');
-    console.log(fileContent);
+    const fileNamePattern=process.argv[2];
+    const filesNameArray=await glob(fileNamePattern);
+    for(const file of filesNameArray){
+        const fileContent=await fs.readFile(file,"utf-8");
+        console.log(fileContent);
+    }
+
+    
  }
  readFile();
