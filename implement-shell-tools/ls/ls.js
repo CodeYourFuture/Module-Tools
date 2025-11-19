f0dde4b1340bb6d53f076f0d6559d536748d408d
sheida-shab <shabankary@gmail.com>
2025-11-19 20:14:53 +0000

    Filtering hidden files name

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -1,14 +1,15 @@
 import { promises as fs } from "node:fs";
 async function listDir() {
-    const flag = process.argv[2];
-    const fileNamePattern = process.argv[3] || process.cwd();
-    console.log(fileNamePattern);
-    console.log(flag);
+   const flag = process.argv[2];
+   const fileNamePattern = process.argv[3] || process.cwd();
+    //console.log(fileNamePattern);
+    //console.log(flag);
    const fileListArray = await fs.readdir(fileNamePattern);
-   console.log(fileListArray);
-  
+   const visibleFileListArray=fileListArray.filter(file => !file.startsWith('.'));
+   //console.log(fileListArray);
+   //console.log(visibleFileListArray);
    
-   for(const item of fileListArray){
+   for(const item of visibleFileListArray){
     console.log(item);
    }
   
