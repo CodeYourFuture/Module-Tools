de2cbfb1f13db1e49d8cdd146c3f88a3b7a6ed7a
sheida-shab <shabankary@gmail.com>
2025-11-19 21:07:59 +0000

    implementing -a flag

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -1,15 +1,24 @@
 import { promises as fs } from "node:fs";
 async function listDir() {
+   let finalFileList;
    const flag = process.argv[2];
-   const fileNamePattern = process.argv[3] || process.cwd();
-    //console.log(fileNamePattern);
-    //console.log(flag);
-   const fileListArray = await fs.readdir(fileNamePattern);
-   const visibleFileListArray=fileListArray.filter(file => !file.startsWith('.'));
-   //console.log(fileListArray);
-   //console.log(visibleFileListArray);
+   const argvArray=process.argv.slice(2);
    
-   for(const item of visibleFileListArray){
+   const flags=argvArray.filter(item => item.startsWith('-'));
+  
+   const Path =
+   argvArray.filter((item) => !item.startsWith("-")) || process.cwd();
+    
+   const fileListArray = await fs.readdir(Path[0] || process.cwd());
+   if (flags.includes('-a')){
+    finalFileList=fileListArray;
+   }else{
+   finalFileList=fileListArray.filter(file => !file.startsWith('.'));
+   }
+   
+  
+   
+   for(const item of finalFileList){
     console.log(item);
    }
   
