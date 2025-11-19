b91a308a3339c3b1ffd9ce24c88bd274d2592f29
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2025-11-19 14:27:30 +0000

    Add -a flag to ls to show hidden files

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -6,6 +6,10 @@ program
   .name("ls")
   .description("list directory contents")
   .option("-1, --one", "Force output to be one entry per line")
+  .option(
+    "-a, --all",
+    "shows all the files including the hidden ones which start with a dot"
+  )
   .argument("[directory]", "Directory to list", "."); // "." means current directory
 
 //interpret the program
@@ -18,11 +22,22 @@ const directory = program.args[0] || ".";
 //read the directory to get array of filenames
 const files = await fs.readdir(directory);
 
+//check for flags
+const hasAflag = program.opts().all;
+
+// Filter the files array BEFORE looping
+// If hasAflag is true, keep all files
+// If hasAflag is false, remove files that start with "."
+
+const fileToShow = hasAflag 
+? files
+: files.filter(file => !file.startsWith("."))
 
 
 //print each file on its own line
 // Note: console.log(files) would print the entire array like: ['file1', 'file2']
 // Loop prints each individually on separate lines
-for (const file of files) {
+
+for (const file of fileToShow) {
     console.log(file)
 }
