49b4e4be59c3c7b0294a6b830479360e189787ef
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-11-21 19:57:28 +0000

    Initialize program with basic info (name, description, argument & options)

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -1,3 +1,10 @@
 import process from "node:process";
 import { promises as fs } from "node:fs";
 import { program } from "commander";
+
+program
+  .name("list-files-in-directory")
+  .description("List all files and directories in a directory")
+  .argument("<path>", "The file path to process")
+  .option("-1", "Output one entry per line")
+//   .option("-a", "List all files & directories, including hidden ones");
