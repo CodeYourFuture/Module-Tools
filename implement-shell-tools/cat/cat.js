fa47ca2eec80c0ddbc58437b244f22f6636d1a47
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-11-21 19:00:06 +0200

    basic implementation of cat with 1 file

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -3,40 +3,18 @@ import { promises as fs } from "node:fs";
 import process from "node:process";
 
 program
-  .name("count-containing-words")
-  .description("Counts words in a file that contain a particular character, optionally gives word or character counts.")
+  .name("node-cat")
+  .description("A Node.js implementation of the Unix cat command")
   .option("-c, --char <char>", "The character to search for", "e")
   .option("-n, --count [type]", "Count type: words or chars")
-  .argument("<path>", "The file path to process");
+  .argument("<path...>", "The file path to process");
 
 program.parse();
 
 const argv = program.args;
-if (argv.length != 1) {
-  console.error(
-    `Expected exactly 1 argument (a path) to be passed but got ${argv.length}.`
-  );
-  process.exit(1);
-}
 
 const path = argv[0];
-// const { char, count } = program.opts();
-console.log(path);
 
 const content = await fs.readFile(path, "utf-8");
 
-const countOfWordsContainingChar = content
-  .split(/\s+/)
-  .filter((word) => word.includes(char)).length;
-
-if (count) {
-  if (count === "words") {
-    const countOfWords = content.split(" ").length;
-    console.log(countOfWordsContainingChar, countOfWords);
-  } else if (count === "chars") {
-    const countOfChars = content.length;
-    console.log(countOfWordsContainingChar, countOfChars);
-  }
-} else {
-  console.log(countOfWordsContainingChar);
-}
+console.log(content.trim());
