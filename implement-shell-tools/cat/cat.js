0931aea909bf0a6b7a9982694e96deea341ffb77
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-11-21 09:01:05 +0000

    checks if our program receieves 1 argument otherwise throw an error message and exit the program (unsuccessful).

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -1,3 +1,8 @@
 import process from "node:process";
 import {promises as fs} from "node:fs";
 
+const argv = process.argv.slice(2);
+if (argv.length != 1) {
+    console.error("Expected exactly 1 argument or a path but received ${argv.length.");
+    process.exit(1);
+}
