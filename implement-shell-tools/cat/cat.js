65c7e6ba88f8106bb9c3f777fa8ec4cb3a101fd5
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-11-21 20:17:54 +0200

    -n flag implemented

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -5,19 +5,39 @@ import process from "node:process";
 program
   .name("node-cat")
   .description("A Node.js implementation of the Unix cat command")
-  .option("-c, --char <char>", "The character to search for", "e")
-  .option("-n, --count [type]", "Count type: words or chars")
+  .option("-n, --number", "Number all output lines")
+  .option(
+    "-b, --numberNonBlank",
+    "Numbers only non-empty lines. Overrides -n option"
+  )
   .argument("<path...>", "The file path to process");
 
 program.parse();
 
 const paths = program.args;
+const { number, numberNonBlank } = program.opts();
 
-let content;
+console.log(number, numberNonBlank);
+
+let content,
+  output,
+  count = 1;
+
 for (const path of paths) {
   content
     ? (content += await fs.readFile(path, "utf-8"))
     : (content = await fs.readFile(path, "utf-8"));
 }
 
-console.log(`${content.trim()}`);
+output = content.trim();
+
+if (number) {
+  const outputArr = output.split("\n");
+  for (let i = 0; i < outputArr.length; i++) {
+    outputArr[i] = `${String(i + 1).padStart(6, " ")}  ${outputArr[i]}`;
+  }
+  console.log(outputArr.join("\n"));
+}
+
+
+// console.log(output);
