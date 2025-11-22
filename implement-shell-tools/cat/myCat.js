2691eee439e243fe01de0d3711506fbd6526fa79
Aida Eslami <ayda.eslami@gmail.com>
2025-11-22 14:37:50 +0000

    Changed basic and addedn flan -n and -b

--- implement-shell-tools/cat/myCat.js
+++ implement-shell-tools/cat/myCat.js
@@ -1,9 +1,41 @@
-import fs from "node:fs";
+import { program } from "commander";
+import { promises as fs } from "node:fs";
+import process from "node:process";
 
-const args = process.argv.slice(2);
+program
+  .name("myCat")
+  .description("Simple file viewer")
+  .option("-n", "Number all output lines")
+  .option("-b", "Number non-blank output lines")
+  .argument("<path...>", "One or more file paths to show");
+
+program.parse();
+
+const args = program.args;
 const files = args;
+const opts = program.opts();
+let lineNumber = 1;
 
 for (const filename of files) {
-  const content = fs.readFileSync(filename, "utf-8");
-  process.stdout.write(content);
+  const content = await fs.readFile(filename, "utf-8");
+
+  if (opts.n) {
+    const lines = content.split("\n");
+
+    for (const line of lines) {
+      console.log(lineNumber + " " + line);
+      lineNumber++;
+    }
+  } else if (opts.b) {
+    const lines = content.split("\n");
+
+    for (const line of lines) {
+      if (line.trim() !== "") {
+        console.log(lineNumber + " " + line);
+        lineNumber++;
+      }
+    }
+  } else {
+    console.log(content);
+  }
 }
