ee0f23e10ecbaffac06cb7803e1f1561b0539350
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-11-24 15:31:36 +0000

    improt "stat" function from the fs module

--- implement-shell-tools/wc/wc.js
+++ implement-shell-tools/wc/wc.js
@@ -1,6 +1,7 @@
 import { program } from "commander";
 import { promises as fs } from "node:fs";
 import process from "node:process";
+import { stat } from "node:fs/promises";
 
 program
   .name("count-containing-lines-words-characters")
