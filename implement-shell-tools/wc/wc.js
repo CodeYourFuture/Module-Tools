c58db9c9075cc7aa5e5e45154ea3248655748c20
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-11-24 18:23:07 +0000

    remove the validation for if we have multiple arguments as this clashes when handle wc for a directory with mutiple files

--- implement-shell-tools/wc/wc.js
+++ implement-shell-tools/wc/wc.js
@@ -13,12 +13,7 @@ program.parse();
 
 const argv = program.args;
 
-if (argv.length != 1) {
-  console.error(
-    `Expected exactly 1 argument (a path) to be passed but got ${argv.length}.`
-  );
-  process.exit(1);
-}
+
 
 const path = argv[0];
 
