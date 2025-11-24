b87db2638bb6a96bd14b949832017b25fcd11ed2
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-11-24 12:52:03 +0000

    set the program basic info and the first option: -l

--- implement-shell-tools/wc/wc.js
+++ implement-shell-tools/wc/wc.js
@@ -1,3 +1,9 @@
 import { program } from "commander";
 import { promises as fs } from "node:fs";
 import process from "node:process";
+
+program
+  .name("count-containing-lines-words-characters")
+  .description("Counts lines, words or characters in a file (or all files) inside a directory")
+  .option("-l, --line", "The number of lines in each file")
+  .argument("<path>", "The file path to process");
