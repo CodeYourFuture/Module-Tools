e9cc6d044b411eff89ce59f1b1b1b071cebc2acf
ike-agu <ikennaww@yahoo.fr>
2025-11-25 18:22:18 +0000

    Use jq to sum individual scores and all  scores

--- jq/script-06.sh
+++ jq/script-06.sh
@@ -7,4 +7,3 @@ set -euo pipefail
 # Your output should contain 6 lines, each with one word and one number on it.
 # The first line should be "Ahmed 1" with no quotes.
 jq -r '.[] | "\(.name) \(.scores[0])"' scores.json
-
