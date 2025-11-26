95cdb2eb23ae83c90977a48686df05dba20326b8
sheida-shab <shabankary@gmail.com>
2025-11-26 12:09:26 +0000

    using argpars to build  automatic  help

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,17 +1,32 @@
 import cowsay
 import sys
-args=sys.argv[1:]
-print(sys.argv)
-flags=list(filter(lambda arg : arg.startswith("-"),args))
-print(flags)
-if len(flags)==0 : 
-    cowsay.cow(" ".join(sys.argv[1:]))
-elif "--help" in flags or "--h" in flags :
-    print("help!!!!!!!!!!!!!!!!")
-elif "--animal" in flags :
-    animalName=args[args.index("--animal")+1]
-    print(animalName)
-    message=" ".join(args[args.index("--animal")+2:])
-    print(message)
-    if hasattr(cowsay,animalName) :
-        getattr(cowsay,animalName)(message)
+import argparse
+
+parser=argparse.ArgumentParser(prog="cowsay",
+                               description="Make animals say things.")
+parser.add_argument("message",help="The message to say.")
+valid_animals=cowsay.char_names;
+parser.add_argument("--animal",
+                    choices=valid_animals,
+                    help=" The animal to be saying things.")
+
+args=parser.parse_args();
+# args=sys.argv[1:]
+# print(sys.argv)
+# flags=list(filter(lambda arg : arg.startswith("-"),args))
+# print(flags)
+# validAnimals=cowsay.char_names;
+# print(validAnimals)
+
+# if len(flags)==0 : 
+#     cowsay.cow(" ".join(sys.argv[1:]))
+# elif "--help" in flags or "--h" in flags :
+#     print("help!!!!!!!!!!!!!!!!")
+# elif "--animal" in flags :
+#     animalName=args[args.index("--animal")+1]
+#     print(animalName)
+#     message=" ".join(args[args.index("--animal")+2:])
+#     print(message)
+#     if hasattr(cowsay,animalName) :
+#         getattr(cowsay,animalName)(message)
+       
