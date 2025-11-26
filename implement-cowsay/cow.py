f3d6575651a25190bd8b34dc280f252aa64fe985
sheida-shab <shabankary@gmail.com>
2025-11-26 12:26:05 +0000

    code now works with --animal and throw error message

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -8,9 +8,11 @@ parser.add_argument("message",help="The message to say.")
 valid_animals=cowsay.char_names;
 parser.add_argument("--animal",
                     choices=valid_animals,
-                    help=" The animal to be saying things.")
+                    help=" The animal to be saying things.",
+                    default="cow")
 
 args=parser.parse_args();
+getattr(cowsay,args.animal)(args.message)
 # args=sys.argv[1:]
 # print(sys.argv)
 # flags=list(filter(lambda arg : arg.startswith("-"),args))
