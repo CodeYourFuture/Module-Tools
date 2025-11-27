cb495884f310e336a9275a3e51d399d7932a2abd
sheida-shab <shabankary@gmail.com>
2025-11-27 21:06:02 +0000

    improving basic wc

--- implement-shell-tools/wc/wc.py
+++ implement-shell-tools/wc/wc.py
@@ -1,12 +1,28 @@
 import argparse
+import os
 
 parser=argparse.ArgumentParser(prog="wc",usage="implement a simple wc in python")
 parser.add_argument("path",nargs="+")
 args=parser.parse_args()
 
-print(args.path)
+# print(args.path)
 paths=args.path
-for path in paths :
-    with open(path,"r") as f :
+words_count=0
+total_lines=0
+total_words=0
+total_bytes=0
+
+for file in paths :
+    with open(file,"r") as f :
+        bytes_count=os.path.getsize(file)
         lines=f.readlines()
-        print(len(lines) , path)
+        lines_count=len(lines)
+        for line in lines :
+            words_count+=len(line.split())
+        total_lines +=lines_count
+        total_bytes += bytes_count
+        total_words +=words_count    
+        print(lines_count ,words_count , bytes_count, file)
+        words_count=0
+        
+print(total_lines,total_words,total_bytes,"total")        
