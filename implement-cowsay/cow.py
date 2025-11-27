f1b0550d72899d41b4329313f0b8fa3c13cc5723
Sara Tahir <saratahir34@gmail.com>
2025-11-27 14:46:56 +0000

    A working solution to  pass animal names as arguments and make them say things

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,5 +1,23 @@
 import cowsay
-import sys
-# Join all arguments after the script name into one string
-message = " ".join(sys.argv[1:]) or "MOO MOOO"
-cowsay.cow(message)
+import argparse   #helps you collect the words typed in command line 
+
+parser = argparse.ArgumentParser(prog="cowsay", description ="Make Animals say things")
+parser.add_argument(
+    "--animal",
+    choices = (cowsay.char_names),
+    help = "The animal chosen to be saying things",
+    default = "cow"
+    )
+parser.add_argument(
+    "message",
+    nargs="+", #tells argparse that this argument can accept one or more values and collect them into a list.
+    help="The message to say."
+)
+args = parser.parse_args()
+
+getattr(cowsay, args.animal)(" ".join(args.message)) #Python built‑in func that get an attribute from an object by name = cowsay module in our case.
+
+
+
+#we now need to fetch animals from cowsay.char_names
+print(cowsay.char_names)
