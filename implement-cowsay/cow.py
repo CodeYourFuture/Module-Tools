975ff65080010d9b897fe85b3ce198beb1326152
Sara Tahir <saratahir34@gmail.com>
2025-11-27 15:10:11 +0000

    Refactoring the code

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,23 +1,28 @@
 import cowsay
 import argparse   #helps you collect the words typed in command line 
 
-parser = argparse.ArgumentParser(prog="cowsay", description ="Make Animals say things")
-parser.add_argument(
-    "--animal",
-    choices = (cowsay.char_names),
-    help = "The animal chosen to be saying things",
-    default = "cow"
+def main():
+    # Set up command-line parser
+    parser = argparse.ArgumentParser(
+        prog="cowsay",
+        description="Make animals say things"
     )
-parser.add_argument(
-    "message",
-    nargs="+", #tells argparse that this argument can accept one or more values and collect them into a list.
-    help="The message to say."
-)
-args = parser.parse_args()
+    parser.add_argument(
+        "--animal",
+        choices=cowsay.char_names,   # fetch supported animals directly from cowsay
+        default="cow",
+        help="The animal chosen to be saying things"
+    )
+    parser.add_argument(
+        "message",
+        nargs="+",                   # accept one or more words as message 
+        help="The message to say"
+    )
+    args = parser.parse_args()
 
-getattr(cowsay, args.animal)(" ".join(args.message)) #Python built‑in func that get an attribute from an object by name = cowsay module in our case.
+    # Dynamically call the correct cowsay function
+    say = getattr(cowsay, args.animal)
+    say(" ".join(args.message))
 
-
-
-#we now need to fetch animals from cowsay.char_names
-print(cowsay.char_names)
+if __name__ == "__main__":
+    main()
