cdc3bdd43e30a8e5c150d09c254e73631e59db84
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-11-30 21:27:18 +0000

    stored the message written in a variable called message

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -3,11 +3,15 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser(
-    prog="cow";
-    description="Makes animals say things";
+    prog="cow",
+    description="Makes animals say things",
 )
 
 parser.add_argument("--animal", help="Select an animal to say anything")
-parser.add_argument("message", nargs ="+" help="The message that the animal says")
+parser.add_argument("message", nargs ="+", help="The message that the animal says")
 
 args = parser.parse_args()
+
+message = " ".join(args.message)
+
+
