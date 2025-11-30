f39e5b5f8c07e28368b5566143b59b561d044496
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-11-30 22:11:41 +0000

    update the --help option content to show the range of animal options an end user can select from

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -2,12 +2,14 @@ import cowsay
 import sys
 import argparse
 
+listed_animals = [listed_animal for listed_animal in dir(cowsay) if callable(getattr(cowsay, listed_animal)) and not listed_animal.startswith("__") and listed_animal not in ["draw", "func", "get_output_string", "CowsayError"]]
+
 parser = argparse.ArgumentParser(
     prog="cow",
     description="Makes animals say things",
 )
 
-parser.add_argument("--animal", help="Select an animal to say anything")
+parser.add_argument("--animal", help="Select an animal to say anything", choices=listed_animals)
 parser.add_argument("message", nargs ="+", help="The message that the animal says")
 
 args = parser.parse_args()
@@ -15,7 +17,6 @@ args = parser.parse_args()
 message = " ".join(args.message)
 animal = args.animal or "cow"
 
-listed_animals = [listed_animal for listed_animal in dir(cowsay) if callable(getattr(cowsay, listed_animal)) and not listed_animal.startswith("__") and listed_animal not in ["draw", "func", "get_output_string", "CowsayError"]]
 
 if animal not in listed_animals:
     print(f"Error: argument --animal: invalid choice: '{animal}'. Choose from: {', '.join(listed_animals)}")
