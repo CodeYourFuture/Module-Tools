e93d1f96f16171faf39e7b42953ab19f5e6d6c94
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-12-01 15:34:24 +0000

    corrected logic to handle several files/path arguments

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -52,31 +52,32 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("-n", help="Number the output lines", action="store_true")
 parser.add_argument("-b", help="Number the non-blank output lines", action="store_true")
-parser.add_argument("path", help="The file to read from")
+parser.add_argument("paths", help="The file(s)/path(s) to read from", nargs="+")
 
 args = parser.parse_args()
 
 line_number = 1
 
-with open(args.path, "r") as f:
-    content = f.read()
+for path in args.paths:
+    with open(path, "r") as f:
+        content = f.read()
 
-    lines = content.split("\n")
-    if lines[len(lines) - 1] == "":
-        lines.pop()
+        lines = content.split("\n")
+        if lines[len(lines) - 1] == "":
+            lines.pop()
 
-    for line in lines:
-        if args.n:
-            print(f"{line_number} {line}")
-            line_number += 1
-        elif args.b:
-            if line != "":
+        for line in lines:
+            if args.n:
                 print(f"{line_number} {line}")
                 line_number += 1
+            elif args.b:
+                if line != "":
+                    print(f"{line_number} {line}")
+                    line_number += 1
+                else:
+                    print(f"{line}")
             else:
                 print(f"{line}")
-        else:
-            print(f"{line}")
 
 
 
