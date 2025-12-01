0a68b74db95e4f6f7b171f3936bf00da4bcd9fe7
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-12-01 16:43:04 +0000

    remove js implementation of ls tool

--- implement-shell-tools/ls/ls.py
+++ implement-shell-tools/ls/ls.py
@@ -1,36 +1,3 @@
-# import process from "node:process";
-# import { promises as fs } from "node:fs";
-# import { program } from "commander";
-
-# program
-#   .name("list-files-in-directory")
-#   .description("List all files and directories in a directory")
-#   .argument("<path>", "The file path to process")
-#   .option("-1, --one", "Output one entry per line")
-#   .option("-a", "List all files & directories, including hidden ones");
-
-# program.parse();
-
-# const path = program.args[0];
-
-# const options = program.opts();
-
-# const directoryContent = await fs.readdir(path);
-
-# let allContent = directoryContent;
-
-# if (!options.a) {
-#     allContent = directoryContent.filter(name => !name.startsWith("."));
-# }
-
-# for (const item of allContent) {
-#     if (options.one) {
-#         process.stdout.write(item + "\n");
-#     } else {
-#         process.stdout.write(item + "  ");
-#     }
-#     }
-
 import argparse
 import os
 
