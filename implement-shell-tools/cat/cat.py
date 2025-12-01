3383e76ac5286c4ae9dca23b9a91334f912953d6
Aida Eslami <ayda.eslami@gmail.com>
2025-12-01 13:32:47 +0000

    My Cat in Python

---
+++ implement-shell-tools/cat/cat.py
@@ -0,0 +1,30 @@
+import argparse
+parser = argparse.ArgumentParser(
+    prog="count-containing-words",
+    description="Counts words in a file that contain a particular character",
+)
+
+counterNumber = 1
+parser.add_argument("-n", action="store_true", help="number all lines")
+parser.add_argument("-b", action="store_true", help="The character to search for" )
+parser.add_argument("path", nargs="+", help="The file to search")
+
+args = parser.parse_args()
+
+for file_path in args.path:
+    with open(file_path, "r") as f:
+        content = f.read()
+        arrText = content.split("\n")
+
+        if args.b:
+            for i in range(len(arrText )):
+                if arrText[i].strip() != "":
+                    print(counterNumber,arrText[i])
+                    counterNumber += 1
+
+        elif args.n:
+          for i in range(len(arrText )):
+              print(counterNumber, arrText[i])
+              counterNumber += 1
+        else:
+            print(content)
