adec5eada647f081fddcc3cbb82ce2fb2bfc8f13
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-12-02 19:01:26 +0000

    add exercise 1 question

---
+++ prep-exercises/exercise_one.py
@@ -0,0 +1,16 @@
+def half(value):
+    return value / 2 
+
+def double(value):
+    return value * 2 
+
+def second(value):
+    return value[1]
+
+
+print(half(22))
+print(half("hello"))
+print(half("22"))
+
+
+# Predict what double("22") will do. Then run the code and check. Did it do what you expected? Why did it return the value it did?
