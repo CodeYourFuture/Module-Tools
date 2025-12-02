c202adfad30ce4d4e6f22d9276a33afff7d60275
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-12-02 19:11:11 +0000

    answer to Q 1 of exercises

--- prep-exercises/exercise_one.py
+++ prep-exercises/exercise_one.py
@@ -1,16 +1,11 @@
-def half(value):
-    return value / 2 
 
 def double(value):
-    return value * 2 
+    return value * 2
 
-def second(value):
-    return value[1]
-
-
-print(half(22))
-print(half("hello"))
-print(half("22"))
+print(double("22"))
 
 
 # Predict what double("22") will do. Then run the code and check. Did it do what you expected? Why did it return the value it did?
+
+# Initial prediction was that a type coercion may take place and  string "22"  will convert to integer 22 and thus the function returns 44.
+# After running the code, I now know that it did not do what i expected. It duplicated/repeated the string and returned 2222.
