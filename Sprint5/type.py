7dbbd28b111c233df6dc43c09b4f132e832df297
Sara Tahir <saratahir34@gmail.com>
2025-12-02 12:20:01 +0000

    Fixed all bug, also ran mypy with --strict flag to pick every issue. All bugs have been resolved.

--- Sprint5/type.py
+++ Sprint5/type.py
@@ -1,6 +1,6 @@
 
 from typing import Dict
-def open_account(balances: Dict[str,int], name:str, amount:int): #dic type str and int
+def open_account(balances: Dict[str,int], name:str, amount:int)->None: #dic type str and int--- returns none/figured after running --strict flag with mypy
     #Adding a new account 
     balances[name] = amount
 
@@ -15,10 +15,8 @@ def format_pence_as_string(total_pence: int)-> str: #type int returns type str
     #formatting pence as a string 
     if total_pence < 100:
         return f"{total_pence}p"
-    pounds =int(total_pence / 100)
-    reveal_type(pounds)  
+    pounds =(total_pence // 100)  #floordivision always returns an int
     pence = total_pence % 100
-    reveal_type(pence)
     return f"£{pounds}.{pence:02d}"
 
 balances:Dict[str,int]  = {             #dic type {str:int}
@@ -27,11 +25,10 @@ balances:Dict[str,int]  = {             #dic type {str:int}
     "Georg": 831,
 }
 
-open_account("Tobi", 9.13) # balances not passed, amount should be an int and not a float/ string
-open_account("Olya", "£7.13")
+open_account(balances,"Tobi", 913) # correct passing of the argumenst to functions with balances.
+open_account(balances,"Olya", 713)
 
 total_pence = sum_balances(balances)  #returntype int
-reveal_type(total_pence)
-total_string = format_pence_as_str(total_pence) #returntype str and wrong function name
-reveal_type(total_string)
+total_string = format_pence_as_string(total_pence) #returntype str and wrong function name
+
 print(f"The bank accounts total {total_string}")
