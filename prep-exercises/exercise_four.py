a70ac33fa37d05868fd9767168bc2999c5c912d7
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-12-03 14:47:39 +0000

    create .py file for exercise 4

---
+++ prep-exercises/exercise_four.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name: str, age: int, preferred_operating_system: str):
+        self.name = name
+        self.age = age
+        self.preferred_operating_system = preferred_operating_system
+
+imran = Person("Imran", 22, "Ubuntu")
+print(imran.name)
+print(imran.address)
+
+eliza = Person("Eliza", 34, "Arch Linux")
+print(eliza.name)
+print(eliza.address)
+
+# Save the above code to a file, and run it through mypy.
+
+#Read the error, and make sure you understand what it’s telling you.
