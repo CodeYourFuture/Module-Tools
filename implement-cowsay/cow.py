9152cdd2b0ae7254285ae36798a076a47bbc29e6
Aida Eslami <ayda.eslami@gmail.com>
2025-12-03 11:22:12 +0000

    create cos.py file and implement cowsay madule

---
+++ implement-cowsay/cow.py
@@ -0,0 +1,22 @@
+import cowsay
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="cowsay",
+    description="Simple cowsay clone",
+)
+parser.add_argument("--animal",choices=cowsay.list_cows(), help="The animal to be saying things.")
+
+parser.add_argument("message",nargs="+", help="Message to be displayed by the cow")
+args = parser.parse_args()
+
+
+msg=" ".join(args.message)
+
+
+msg = " ".join(args.message)
+
+if args.animal is None:
+    print(cowsay.cowsay(msg))
+else:
+   print(cowsay.cowsay(msg, cow=args.animal))
