bd4f33ec01c82efabcbda6179fe14f0670ec5a9a
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-12-04 11:09:28 +0200

    Code added into main function as per Python best practice

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,26 +1,27 @@
 import cowsay
 import argparse
 
-# Get dynamic list of animals from the cowsay library
-animals = cowsay.char_names
+def main():
+    # Get dynamic list of animals from the cowsay library
+    animals = cowsay.char_names
 
-parser = argparse.ArgumentParser(
-    prog="cowsay",
-    description="Make animals say things"
-)
+    parser = argparse.ArgumentParser(
+        prog="cowsay",
+        description="Make animals say things"
+    )
 
-parser.add_argument("--animal", choices=animals, help="The animal to be saying things.")
-parser.add_argument("message", nargs="+", help="The message to say.")
+    parser.add_argument("--animal", choices=animals, help="The animal to be saying things.")
+    parser.add_argument("message", nargs="+", help="The message to say.")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-# Join all message words into a single string
-message = " ".join(args.message)
+    # Join all message words into a single string
+    message = " ".join(args.message)
 
+    if args.animal:
+        cowsay.char_funcs[args.animal](message)
+    else:
+        cowsay.cow(message)
 
-if args.animal:
-    cowsay.char_funcs[args.animal](message)
-else:
-    # if no animal is provided, use default
-    cowsay.cow(message)
-
+if __name__ == "__main__":
+    main()
