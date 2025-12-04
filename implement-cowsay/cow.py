62bd86e53127b7621c4296127aa3550740a7c905
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-12-04 10:50:30 +0200

    Basic logic complete

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,19 +1,32 @@
 import cowsay
 import argparse
 
+animals = cowsay.char_names
+
 parser = argparse.ArgumentParser(
     prog="cowsay",
     description="Make animals say things"
 )
 
-parser.add_argument("--animal", help="The animal to be saying things.")
+parser.add_argument("-a", "--animal", choices=animals, help="The animal to be saying things.")
 parser.add_argument("message", nargs="+", help="The message to say.")
 
 args = parser.parse_args()
 
-# cowsay.cow(" ".join(sys.argv[1:]))
+message = " ".join(args.message)
+
+print(args)
+
+if args.animal:
+    # cowsay[args.animal](message)
+    print(cowsay.get_output_string(args.animal, message))
+    # print('hi')
+else:
+    # print(cowsay.get_output_string('cow', 'Hello World'))
+    cowsay.cow(message) # if no animal is provided, use default
 
 
-animals = cowsay.char_names
 
 print(animals)
+
+ 
