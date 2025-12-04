842c004712813755343a0dcdc889fced6f30cda5
Sara Tahir <saratahir34@gmail.com>
2025-12-04 21:58:08 +0000

    Removed the error causing lines

--- Sprint5/lastexer.py
+++ Sprint5/lastexer.py
@@ -1,24 +1,31 @@
 class Parent:
+    """Represents a person with a first and last name."""
     def __init__(self, first_name: str, last_name: str):
         self.first_name = first_name
         self.last_name = last_name
 
-    def get_name(self) -> str:  # get name is a method of class parent 
+    def get_name(self) -> str:
+        """Return the full name as 'First Last'."""
         return f"{self.first_name} {self.last_name}"
 
 
-class Child(Parent):   #childclass of parent
+class Child(Parent):   
+    """Represents a person who can change last names, tracking previous ones."""
     def __init__(self, first_name: str, last_name: str):
-        super().__init__(first_name, last_name)  #superclass constructor intialised in childclass
-        self.previous_last_names = []   #intialised a new attribute
+        """Initialize a Child with a first and last name, plus a list of previous last names."""
+        super().__init__(first_name, last_name) 
+        self.previous_last_names: list[str] = []  
 
     def change_last_name(self, last_name) -> None:
-        self.previous_last_names.append(self.last_name) #append previous lastname to the previous_last_names[]
-        self.last_name = last_name #set the new one as last_name
+        """Change the last name and record the previous one."""
+        self.previous_last_names.append(self.last_name)
+        self.last_name = last_name
 
     def get_full_name(self) -> str:
+        """Return the full name, with suffix showing original last name if changed."""
         suffix = ""
-        if len(self.previous_last_names) > 0:  #if last_name has been changes, it adds the previous last name as a suffix to the new one 
+        suffix = ""
+        if len(self.previous_last_names) > 0:
             suffix = f" (née {self.previous_last_names[0]})"
         return f"{self.first_name} {self.last_name}{suffix}"
 
@@ -31,7 +38,3 @@ print(person1.get_full_name())
 
 person2 = Parent("Elizaveta", "Alekseeva")
 print(person2.get_name())
-print(person2.get_full_name())  #childclass method
-person2.change_last_name("Tyurina") #childclass method
-print(person2.get_name())
-print(person2.get_full_name())
