8671d12f61bfbd034e1708ecfa37eb5eaa2ef8bd
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-12-04 10:21:46 +0200

    argparse setup for project

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,1 +1,19 @@
 import cowsay
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="cowsay",
+    description="Make animals say things"
+)
+
+parser.add_argument("--animal", help="The animal to be saying things.")
+parser.add_argument("message", nargs="+", help="The message to say.")
+
+args = parser.parse_args()
+
+# cowsay.cow(" ".join(sys.argv[1:]))
+
+
+animals = cowsay.char_names
+
+print(animals)
