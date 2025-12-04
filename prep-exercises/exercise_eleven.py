665b5d302be372815135a1c9bdaeb147c5401018
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-12-04 20:02:31 +0000

    add question 11 as a comment

---
+++ prep-exercises/exercise_eleven.py
@@ -0,0 +1,11 @@
+#Write a program which:
+#1. Already has a list of Laptops that a library has to lend out.
+#2. Accepts user input to create a new Person - it should use the input function to read a person’s
+#name, age, and preferred operating system.
+#3.Tells the user how many laptops the library has that have that operating system.
+#4.If there is an operating system that has more laptops available, tells the user that if they’re willing to
+#accept that operating system they’re more likely to get a laptop.
+
+#You should convert the age and preferred operating system input from the user into more constrained
+#types as quickly as possible, and should output errors to stderr and terminate the program with a non-
+#zero exit code if the user input bad values.
