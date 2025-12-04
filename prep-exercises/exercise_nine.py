d4ab96563394da5a19f9b2b24363796aec80ff81
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-12-04 13:49:12 +0000

    added "age" field and updated class instances accodingly

--- prep-exercises/exercise_nine.py
+++ prep-exercises/exercise_nine.py
@@ -4,12 +4,16 @@ from typing import List
 @dataclass(frozen=True)
 class Person:
     name: str
+    #Add "age" attribute
+    age: int
     children: List["Person"]
 
-fatma = Person(name="Fatma", children=[])
-aisha = Person(name="Aisha", children=[])
+#Add "age" field and thier respective values to "fatma" and "aisha" - instances of class "Person"
+fatma = Person(name="Fatma", age=14, children=[])
+aisha = Person(name="Aisha",age=22, children=[])
 
-imran = Person(name="Imran", children=[fatma, aisha])
+##Add "age" field and its values to "imran" -  instance of class "Person"
+imran = Person(name="Imran", age=44, children=[fatma, aisha])
 
 def print_family_tree(person: Person) -> None:
     print(person.name)
