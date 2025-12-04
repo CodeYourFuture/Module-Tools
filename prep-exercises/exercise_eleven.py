0cac4f29391b6f6fd489af6558ba9dbdcfece3a2
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-12-04 20:33:20 +0000

    update values for laptop instances

--- prep-exercises/exercise_eleven.py
+++ prep-exercises/exercise_eleven.py
@@ -32,11 +32,11 @@ class Laptop:
 
 laptops = [
     Laptop(id=1, manufacturer="HP", model="Chromebook Plus 514", screen_size_in_inches=14, operating_system="ChromeOS"),
-    Laptop(id=2, manufacturer="Microsoft", model="XPS", screen_size_in_inches=13, operating_system="Windows"),
+    Laptop(id=2, manufacturer="Microsoft", model="XPS", screen_size_in_inches=13.8, operating_system="Windows"),
     Laptop(id=3, manufacturer="Apple", model="MacBook Air", screen_size_in_inches=15, operating_system="macOS"),
-    Laptop(id=4, manufacturer="Lenovo", model="ThinkPad X220", screen_size_in_inches=12, operating_system="Linux"),
-    Laptop(id=5, manufacturer="Dell", model="XPS", screen_size_in_inches=13, operating_system="Arch Linux"),
-    Laptop(id=6, manufacturer="Dell", model="XPS", screen_size_in_inches=15, operating_system="Ubuntu"),
+    Laptop(id=4, manufacturer="Lenovo", model="ThinkPad X220", screen_size_in_inches=12.5, operating_system="Linux"),
+    Laptop(id=5, manufacturer="Dell", model="XPS", screen_size_in_inches=13.7, operating_system="Arch Linux"),
+    Laptop(id=6, manufacturer="Dell", model="XPS", screen_size_in_inches=15.5, operating_system="Ubuntu"),
     Laptop(id=7, manufacturer="Dell", model="XPS", screen_size_in_inches=15, operating_system="ubuntu"),
-    Laptop(id=8, manufacturer="Apple", model="MacBook", screen_size_in_inches=13, operating_system="macOS"),
+    Laptop(id=8, manufacturer="Apple", model="MacBook", screen_size_in_inches=13.3, operating_system="macOS"),
 ]
