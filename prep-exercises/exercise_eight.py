dbe6de68e50a2b76c70cc0baa041031a83e99e0b
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-12-04 11:34:51 +0000

    reconstructing the person class as per @dataclass

--- prep-exercises/exercise_eight.py
+++ prep-exercises/exercise_eight.py
@@ -1,14 +1,13 @@
 from datetime import date
 from dataclasses import dataclass
 
-
+@dataclass
 class Person:
-    def __init__(self, name: str, date_of_birth: date, preferred_operating_system: str):
-        self.name = name
-        self.preferred_operating_system = preferred_operating_system
-        #Add "date_of_birth" attribute to the "Person" class
-        self.date_of_birth = date_of_birth
-
+        name: str
+        date_of_birth: date
+        preferred_operating_system: str
+        
+        
     def is_adult(self) ->bool:
         age_in_days = (date.today() - self.date_of_birth).days 
         age_in_years = age_in_days / 365.2425
