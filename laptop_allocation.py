86a77ddfc4d633c7664f94a7a6b03ccc6337adee
sheida-shab <shabankary@gmail.com>
2025-12-07 16:16:15 +0000

    Added a few comments

--- laptop_allocation.py
+++ laptop_allocation.py
@@ -2,10 +2,12 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Dict, List
 
+# Define available operating systems
 class OperatingSystem(Enum):
     MACOS = "macOS"
     ARCH = "Arch Linux"
     UBUNTU = "Ubuntu"
+
 
 @dataclass(frozen=True)
 class Person:
@@ -23,6 +25,7 @@ class Laptop:
     screen_size_in_inches: float
     operating_system: OperatingSystem
 
+# List of available laptops
 laptops = [
     Laptop(id=1, manufacturer="Dell", model="XPS", screen_size_in_inches=13, operating_system=OperatingSystem.ARCH),
     Laptop(id=2, manufacturer="Dell", model="XPS", screen_size_in_inches=15, operating_system=OperatingSystem.UBUNTU),
@@ -31,6 +34,8 @@ laptops = [
     Laptop(id=5, manufacturer="Apple", model="macBook", screen_size_in_inches=13, operating_system=OperatingSystem.MACOS),
     Laptop(id=6, manufacturer="Apple", model="macBook", screen_size_in_inches=13, operating_system=OperatingSystem.ARCH),
 ] 
+
+# List of people to allocate laptops to
 people = [
     Person(name="Imran", age=22, preferred_operating_system=[OperatingSystem.ARCH,OperatingSystem.UBUNTU]),
     Person(name="Eliza", age=34, preferred_operating_system=[OperatingSystem.ARCH,OperatingSystem.MACOS,OperatingSystem.UBUNTU]),
@@ -38,7 +43,11 @@ people = [
     Person(name="Mary", age=35, preferred_operating_system=[OperatingSystem.MACOS,OperatingSystem.ARCH]),
     Person(name="Sara", age=28, preferred_operating_system=[OperatingSystem.MACOS])
 ]
+
+# Global sadness counter
 sadness=0
+
+# Allocate laptops to people to minimize total sadness
 def allocate_laptops(people: List[Person], laptops: List[Laptop]) -> Dict[Person, Laptop]: 
     sorted_people_OS_count=sorted(people,key=lambda p:len(p.preferred_operating_system))
     allocated_history : Dict[Person,Laptop] ={}
@@ -48,19 +57,18 @@ def allocate_laptops(people: List[Person], laptops: List[Laptop]) -> Dict[Person
      for i in range(len(person.preferred_operating_system)) :
           for laptop in laptops :
               if person.preferred_operating_system[i] == laptop.operating_system :
-                  allocated_history[person.name]=laptop
-                #   print(person.name,laptop.id,laptop.operating_system,i)
-                  sadness += i
+                  allocated_history[person.name]=laptop # assign laptop
+                  sadness += i  # increment sadness by preference index
                   laptops.remove(laptop)
                   allocated_flag=True
                   break
           if allocated_flag : 
             break     
           
-     if not allocated_flag :
+     if not allocated_flag :   # assign any remaining laptop if preferred OS not found
         allocated_history[person.name]=laptops[0]
         laptops.remove(laptops[0])
-        sadness +=100       
+        sadness +=100   # high sadness for non-preferred OS
               
     return allocated_history          
 
