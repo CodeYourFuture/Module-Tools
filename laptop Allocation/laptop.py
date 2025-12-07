04009aaa5175a36335a1399b6d67eff95022159e
Sara Tahir <saratahir34@gmail.com>
2025-12-07 19:42:13 +0000

    Using Hungarian Algorithm for laptop allocation to minimize sadness.

--- laptop Allocation/laptop.py
+++ laptop Allocation/laptop.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Dict, List
 import sys
+import numpy as np  #to build and mnaiplate our sadness grid
+from scipy.optimize import linear_sum_assignment #to find optimal laptop allocaation to minimize sadness across all users
 
 class OperatingSystem(Enum):
     MACOS = "macOS"
@@ -37,13 +39,13 @@ laptop = [
 ]
 # Preset dataset of people
 people = [
-    Person(name="Sara", age=31, preferred_operating_system=[OperatingSystem.ARCH,OperatingSystem.UBUNTU]),
-    Person(name="Shabs", age=40, preferred_operating_system=[OperatingSystem.ARCH,OperatingSystem.MACOS,OperatingSystem.UBUNTU]),
-    Person(name="Jawad", age= 36, preferred_operating_system=[OperatingSystem.MACOS,OperatingSystem.UBUNTU,OperatingSystem.ARCH]),    
-    Person(name="Mike", age=35, preferred_operating_system=[OperatingSystem.MACOS,OperatingSystem.ARCH]),
-    Person(name="Mawra", age=28, preferred_operating_system=[OperatingSystem.MACOS]),
-    Person(name="Fatma", age= 22, preferred_operating_system=[OperatingSystem.UBUNTU,OperatingSystem.ARCH]),    
-    Person(name="Muhib", age= 19, preferred_operating_system=[OperatingSystem.MACOS,OperatingSystem.UBUNTU]),    
+    Person(name="Sara", age=31, preferred_operating_systems=[OperatingSystem.ARCH,OperatingSystem.UBUNTU]),
+    Person(name="Shabs", age=40, preferred_operating_systems=[OperatingSystem.ARCH,OperatingSystem.MACOS,OperatingSystem.UBUNTU]),
+    Person(name="Jawad", age= 36, preferred_operating_systems=[OperatingSystem.MACOS,OperatingSystem.UBUNTU,OperatingSystem.ARCH]),    
+    Person(name="Mike", age=35, preferred_operating_systems=[OperatingSystem.MACOS,OperatingSystem.ARCH]),
+    Person(name="Mawra", age=28, preferred_operating_systems=[OperatingSystem.MACOS]),
+    Person(name="Fatma", age= 22, preferred_operating_systems=[OperatingSystem.UBUNTU,OperatingSystem.ARCH]),    
+    Person(name="Muhib", age= 19, preferred_operating_systems=[OperatingSystem.MACOS,OperatingSystem.UBUNTU]),    
 
 ]
 
@@ -57,7 +59,29 @@ def sadness(person: Person, laptop: Laptop) -> int:
     else:
         return 100
     
-#There are two approaches to solve this
-#Greedy approach or Hungarian Algorithm Approach
+#I have chose to use  Hungarian Algorithm Approach to solve the problem.
 
-#def allocate_laptops(people: List[Person], laptops: List[Laptop]) -> Dict[Person, Laptop]:
+def allocate_laptops(people: List[Person], laptops: List[Laptop]) -> Dict[Person, Laptop]:
+    n_people = len(people) #length of people []
+    n_laptops = len(laptops) #length of laptops []
+    if n_laptops < n_people:
+        raise ValueError("Not enough laptops to allocate one to each person.")
+    sadness_matrix = np.zeros((n_people, n_laptops), dtype= int)  #npzero mean each cell in 2d grid is a zero and dtype is datatype int so it int 0
+    for i, person in enumerate(people): #enumerate gives us both index position and the item itself from the list 
+        for j, laptop in enumerate(laptops):
+            sadness_matrix[i,j] = sadness(person, laptop) #this matrix represents complete laptop allocation 
+            # Hungarian algorithm to run on our sadness matrix
+    row_indices, col_indices = linear_sum_assignment(sadness_matrix) #which person which laptop
+        # Map people to laptops
+    allocation = {}
+    for i, j in zip(row_indices, col_indices):#it pairs up results (person indices and laptop indices). The loop then uses those pairs to build the final allocation dictionary
+        allocation[people[i].name] = laptops[j]
+    return allocation
+
+
+def print_allocation(allocation: Dict[Person, Laptop]):
+    for name, lap in allocation.items():
+        print(f"{name} gets Laptop {lap.id} ({lap.operating_system.value})")
+
+allocation = allocate_laptops(people, laptop)
+print_allocation(allocation)
