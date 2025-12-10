f81a6d001095903bb66533e65ba8ef4e35edf3b1
Aida Eslami <ayda.eslami@gmail.com>
2025-12-10 11:08:14 +0000

    Enum

---
+++ type/myEnum.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+class OperatingSystem(Enum):
+    MACOS = "macOS"
+    ARCH = "Arch Linux"
+    UBUNTU = "Ubuntu"
+
+@dataclass(frozen=True)
+class Person:
+    name: str
+    age: int
+    preferred_operating_system: OperatingSystem
+
+
+@dataclass(frozen=True)
+class Laptop:
+    id: int
+    manufacturer: str
+    model: str
+    screen_size_in_inches: float
+    operating_system: OperatingSystem
+
+
+
+
+def find_possible_laptops(laptops: List[Laptop], person: Person) -> List[Laptop]:
+    possible_laptops = []
+    for laptop in laptops:
+        if laptop.operating_system == person.preferred_operating_system:
+            possible_laptops.append(laptop)
+    return possible_laptops
+
+
+
+
+laptops = [
+    Laptop(id=1, manufacturer="Dell", model="XPS", screen_size_in_inches=13, operating_system=OperatingSystem.ARCH),
+    Laptop(id=2, manufacturer="Dell", model="XPS", screen_size_in_inches=15, operating_system=OperatingSystem.UBUNTU),
+    Laptop(id=3, manufacturer="Dell", model="XPS", screen_size_in_inches=15, operating_system=OperatingSystem.UBUNTU),
+    Laptop(id=4, manufacturer="Apple", model="macBook", screen_size_in_inches=13, operating_system=OperatingSystem.MACOS),
+]
+
+
+
+namePerson = input("enter your personal name: ")
+try:
+    agePerson = input("enter your age: ")
+except ValueError:
+    print("Invalid age. Age must be a number.")
+    exit()
+try:
+    preferredOS = input("enter your preferred operating system: ")
+except KeyError:
+    print("Invalid operating system entered. Please choose from: MACOS, ARCH, UBUNTU.")
+    exit()
+    
+person=Person(name=namePerson, age=int(agePerson), preferred_operating_system=OperatingSystem[preferredOS])
+possible_laptops = find_possible_laptops(laptops, person)
+print(f"Possible laptops for {person.name}: {possible_laptops}")
+
+
+
