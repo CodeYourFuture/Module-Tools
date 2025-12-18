aa561c70ef58d0a074ec74376e1aa942ccd0efc8
sheida-shab <shabankary@gmail.com>
2025-12-18 17:03:26 +0000

    Add support for -1 flag to control output format

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -1,26 +1,29 @@
 import { promises as fs } from "node:fs";
 async function listDir() {
-   let finalFileList;
-   const flag = process.argv[2];
-   const argvArray=process.argv.slice(2);
-   
-   const flags=argvArray.filter(item => item.startsWith('-'));
-  
-   const Path =
-   argvArray.filter((item) => !item.startsWith("-")) || process.cwd();
-    
-   const fileListArray = await fs.readdir(Path[0] || process.cwd());
-   if (flags.includes('-a')){
-    finalFileList=fileListArray;
-   }else{
-   finalFileList=fileListArray.filter(file => !file.startsWith('.'));
-   }
-   
-  
-   
-   for(const item of finalFileList){
-    console.log(item);
-   }
-  
+  let finalFileList;
+  const flag = process.argv[2];
+  const argvArray = process.argv.slice(2);
+
+  const flags = argvArray.filter((item) => item.startsWith("-"));
+
+  const Path =
+    argvArray.filter((item) => !item.startsWith("-")) || process.cwd();
+
+  const isSingleColumn = flags.includes("-1");
+
+  const fileListArray = await fs.readdir(Path[0] || process.cwd());
+  if (flags.includes("-a")) {
+    finalFileList = fileListArray;
+  } else {
+    finalFileList = fileListArray.filter((file) => !file.startsWith("."));
+  }
+
+  if (isSingleColumn) {
+    for (const item of finalFileList) {
+      console.log(item);
+    }
+  } else {
+    console.log(finalFileList.join(" "));
+  }
 }
 listDir();
