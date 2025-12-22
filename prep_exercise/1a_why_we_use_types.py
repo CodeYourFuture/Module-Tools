35d41f09a850b81e77b3ae17cc16729b1f083ef9
ike-agu <ikennaww@yahoo.fr>
2025-12-22 02:22:30 +0000

    Explain bug in double() function

--- prep_exercise/1a_why_we_use_types.py
+++ prep_exercise/1a_why_we_use_types.py
@@ -1,4 +1,4 @@
-# exercise
+# ========== exercise 1 ==================================
 # Predict what double("22") will do. Then run the code and check. Did it do what you expected? Why did it return the value it did?
 
 def half(value):
