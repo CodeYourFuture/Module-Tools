87ed47e969fd9368c8e41f90f8e821410c406e53
Nataliia Volkova <volkov.nataliia@gmail.com>
2026-01-06 19:10:43 +0000

    correct numbering lines

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -10,18 +10,17 @@ parser.add_argument("-b",  action="store_true", help="Display numbers non-empty 
 parser.add_argument("path", nargs="+", help="The file to search")
 
 args = parser.parse_args()
+number = 1
 
 for file_path in args.path:
     with open(file_path, "r") as f:
         content = f.readlines()
 
     if args.n:
-        number = 1
         for line in content:
             print(f"{number}\t{line.strip()}")
             number +=1
     elif args.b:
-        number = 1
         for line in content:
             if line.strip() !="":
                 print(f"{number}\t{line.strip()}")
@@ -30,13 +29,3 @@ for file_path in args.path:
                 print("")
     else:
         print("".join(content))
-   
-               
-
-
-# if args.b:
-#     number=1
-#     for line in content:
-#         print
-
-# print(content)
