455edb06d4e23c104c25c2b69e974b59bf00d776
Rashaad Ebrahim <rmebrahim@gmail.com>
2026-01-12 09:15:02 +0200

    import from method updated

--- prep-exercises/dataclasses.py
+++ prep-exercises/dataclasses.py
@@ -1,19 +1,19 @@
 # --- Exercise ---
-# Change the Person class to take a date of birth (using the standard library’s datetime.date class) and store it in a field instead of age.
+# Write a Person class using @datatype which uses a datetime.date for date of birth, rather than an int for age.
 
-# Update the is_adult method to act the same as before.
+# Re-add the is_adult method to it.
 
 # --- Solution ---
-from datetime import date
+import datetime
 
 class Person:
-    def __init__(self, name: str, date_of_birth: date, preferred_operating_system: str):
+    def __init__(self, name: str, date_of_birth: datetime.date, preferred_operating_system: str):
         self.name = name
         self.date_of_birth = date_of_birth
         self.preferred_operating_system = preferred_operating_system
 
     def is_adult(person: Person) -> bool:
-        today = date.today()
+        today = datetime.date.today()
         age = today.year - person.date_of_birth.year
         if today.month < person.date_of_birth.month or (today.month == person.date_of_birth.month and today.day < person.date_of_birth.day):
             age -= 1
