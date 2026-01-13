9bc06ec9e6ceec874c786a26b9604ccbc9a698f6
Nataliia Volkova <volkov.nataliia@gmail.com>
2026-01-13 21:50:39 +0000

    refactoring

--- implement-shell-tools/wc/wc.py
+++ implement-shell-tools/wc/wc.py
@@ -11,23 +11,15 @@ parser.add_argument("path", nargs="+", help="The file to search")
 
 args = parser.parse_args()
 
-
-count_total_lines = 0
-count_total_words = 0
-count_total_bytes = 0
-
-for file_path in args.path:
-    with open(file_path, "r") as file:
-        content = file.read()
+def calculate_counts(content):
 
     count_lines = content.count("\n")
     count_words = len(content.split())
     count_bytes = len(content.encode("utf-8"))
+    
+    return count_lines, count_words, count_bytes
 
-    count_total_lines += count_lines
-    count_total_words += count_words
-    count_total_bytes += count_bytes
-
+def format_output(count_lines, count_words, count_bytes, file_path):
     if not (args.l or args.w or args.c):
         print(count_lines, count_words, count_bytes, file_path)
     else:
@@ -40,16 +32,22 @@ for file_path in args.path:
             line += f"{count_bytes}  "
         line += f"{file_path}"
         print(line)
+
+count_total_lines = 0
+count_total_words = 0
+count_total_bytes = 0
+
+for file_path in args.path:
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    count_lines, count_words, count_bytes = calculate_counts(content)
+
+    count_total_lines += count_lines
+    count_total_words += count_words
+    count_total_bytes += count_bytes
+    
+    format_output(count_lines, count_words, count_bytes, file_path)
+    
 if len(args.path) > 1:
-    if not (args.l or args.w or args.c):
-        print(count_total_lines, count_total_words, count_total_bytes, " total")
-    else:
-        line = ""
-        if args.l:
-            line += f"{count_total_lines}  "
-        if args.w:
-            line += f"{count_total_words}  "
-        if args.c:
-            line += f"{count_total_bytes}  "
-        line += " total"
-        print(line)
+    format_output(count_total_lines, count_total_words, count_total_bytes, " total")
