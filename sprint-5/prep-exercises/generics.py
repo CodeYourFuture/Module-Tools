3073be49622eb82173bad03ef08ce41635ce9ced
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-01-16 20:52:36 +0000

    Added blank lines before and after the class declaration for better readability

--- sprint-5/prep-exercises/generics.py
+++ sprint-5/prep-exercises/generics.py
@@ -4,17 +4,24 @@
 #solution:
 #                           
 from dataclasses import dataclass
-from typing import List                     
+from typing import List   
+                  
 @dataclass(frozen=True)
 class Person:
     name: str
     age: int
-    children: List["Person"]                                                        
+    children: List["Person"]   
+
+
 fatma = Person(name="Fatma", age=5, children=[])
 aisha = Person(name="Aisha", age=7, children=[])        
 imran = Person(name="Imran", age=30, children=[fatma, aisha])
+
+
 def print_family_tree(person: Person) -> None:
     print(person.name)
     for child in person.children:
         print(f"- {child.name} ({child.age})")
+
+
 print_family_tree(imran)
