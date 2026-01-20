a90ceffd046b02dd9f86f42d921d47c5a5710a95
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2026-01-20 12:15:04 +0000

    Refactor:  remove repeated code and nested ifs

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -14,22 +14,20 @@ const number = program.opts().n;
 const nonEmptyLine = program.opts().b;
 
 let lineNumber = 1;
- 
+
 for (const path of paths) {
   try {
     const read = await fs.readFile(path, "utf-8");
     const lines = read.split("\n");
+
+    function printNumLine(line) {
+      console.log(`${String(lineNumber).padStart(6, " ")} ${line}`);
+      lineNumber++;
+    }
+
     for (let i of lines) {
-      if (number) {
-        console.log(`${String(lineNumber).padStart(6, " ")} ${i}`);
-        lineNumber++;
-      } else if (nonEmptyLine) {
-        if (i.trim() !== "") {
-          console.log(`${String(lineNumber).padStart(6, " ")} ${i}`);
-          lineNumber++;
-        } else {
-          console.log(i);
-        }
+      if (number || (nonEmptyLine && i.trim() !== "")) {
+        printNumLine(i);
       } else {
         console.log(i);
       }
