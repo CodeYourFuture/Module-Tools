02a58e183d759ba47946606588f11f9d49ffbe3f
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2026-01-20 11:42:56 +0000

    fix: handle multi word text and display animals in help

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -1,17 +1,16 @@
 import argparse
 import cowsay
 
+animals = list(cowsay.char_funcs.keys())
 
 parser = argparse.ArgumentParser(description="implement Cowsay command")
-parser.add_argument("text", help="Text to be displayed")
-parser.add_argument("--animal", help="Animal to say the text", default="cow")
+parser.add_argument("text",nargs="+" ,help="Text to be displayed")
+parser.add_argument("--animal", help=f"Animal to say the text (choices: {', '.join(animals)})", default="cow",choices=animals)
 args = parser.parse_args()
 
-animals = cowsay.char_names
-animal = args.animal.lower()
-if animal not in animals:
-    print(f"Invalid animal. Supported animals are: {', '.join(animals)}")
-    exit(1)
-cowsay.char_funcs[animal](args.text)    
+text = " ".join(args.text)
+animal = args.animal
+
+cowsay.char_funcs[animal](text)    
 
 
