f4cb50707353e965574aabae4fb32aa2f42f32f6
ike-agu <ikennaww@yahoo.fr>
2026-01-20 03:06:01 +0000

    Update print_family_tree to print descendants recursively

--- prep_exercise/6_generics.py
+++ prep_exercise/6_generics.py
@@ -13,8 +13,8 @@ aisha = Person(name="Aisha", age =21, children=[])
 imran = Person(name="Imran", age = 33, children=[fatma, aisha])
 
 def print_family_tree(person: Person) -> None:
-    print(person.name)
+    print(f"- {person.name} ({person.age})")
     for child in person.children:
-        print(f"- {child.name} ({child.age})")
+        print_family_tree(child)
 
 print_family_tree(imran)
