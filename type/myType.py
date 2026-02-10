f4d79b1826aa19f137071b5e7a58d2867420bea5
Aida Eslami <ayda.eslami@gmail.com>
2026-02-10 00:13:23 +0000

    add type hint

--- type/myType.py
+++ type/myType.py
@@ -1,4 +1,4 @@
-def triple(number):
+def triple(number : int)-> int:
     return number * 3
 
 print(triple(10))
@@ -6,20 +6,19 @@ print(triple(10))
 # Bank account functions
 
 
-def open_account( name:str, amount:float):
-    balances["name"] = int(amount * 100  )
+def open_account( name:str, amount:float)->None:
+    balances[name] = int(amount * 100  )
 
 
 
-
-def sum_balances(accounts):
+def sum_balances(accounts: dict[str, int]) -> int:
     total = 0
     for name, pence in accounts.items():
         print(f"{name} had balance {pence}")
         total += pence
     return total
 
-def format_pence_as_string(total_pence):
+def format_pence_as_string(total_pence: int)->str:
     if total_pence < 100:
         return f"{total_pence}p"
     pounds = int(total_pence / 100)
