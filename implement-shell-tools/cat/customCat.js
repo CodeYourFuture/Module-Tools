89daa4f5ad0cd93a574534d15ec11935372b05c6
TzeMingHo <tzemingho@gmail.com>
2026-03-21 19:05:29 +0000

    working on case 5 in cat

--- implement-shell-tools/cat/customCat.js
+++ implement-shell-tools/cat/customCat.js
@@ -6,24 +6,43 @@ program
   .name("custom-cat")
   .description("my-own-version-of-cat")
   .option("-n, --line", "Adding a number before each roll")
-  .argument("<path>", "The file path to process");
+  .option(
+    "-b, --nonBlank",
+    "Only adding a number before roll that is non blank",
+  )
+  .argument("<path...>", "The file path to process");
 
 program.parse();
 
 const options = program.opts();
 
 const argv = program.args;
-if (argv.length != 1) {
-  console.log(`We need exactly 1 path to process but we got ${argv.length}`);
+if (argv.length < 1) {
+  console.log(
+    `We need at least 1 path of file to process but we got ${argv.length}`,
+  );
   process.exit(1);
 }
 
-const path = argv[0];
+const paths = argv;
 
-const context = await fs.readFile(path, "utf-8");
-const contextArray = context.trimEnd().split("\n");
-contextArray.forEach((string, index) => {
-  options.line
-    ? console.log(`     ${index + 1}  ${string}`)
-    : console.log(`${string}`);
-});
+let count = 1;
+
+for (let path of paths) {
+  const context = await fs.readFile(path, "utf-8");
+  if (options.line && options.nonBlank) {
+    context
+      .trimEnd()
+      .split("\n")
+      .forEach((string) => {
+        if (string != null || string != "") {
+          console.log(`     ${count}  ${string}`);
+          count++;
+        } else {
+          console.log(string);
+        }
+      });
+  } else {
+    console.log(context.trimEnd());
+  }
+}
