06aacde8a87e9e8c2ae6c1dad35c4e300569db7f
TzeMingHo <tzemingho@gmail.com>
2026-03-24 14:02:23 +0000

    improved logic for sorting the file order and added function for formatting directory

--- implement-shell-tools/ls/customLs.js
+++ implement-shell-tools/ls/customLs.js
@@ -15,31 +15,53 @@ const argumentsArray = program.args;
 
 const path = argumentsArray[0] || "./";
 
+async function formatFileName(base, fileName) {
+  try {
+    const fullPath = `${base.endsWith("/") ? base : base + "/"}${fileName}`;
+    const stats = await fs.stat(fullPath);
+    if (stats.isDirectory()) {
+      return `\x1b[1;34m${fileName}\x1b[0m`;
+    }
+    return fileName;
+  } catch (error) {
+    return fileName;
+  }
+}
+
 try {
   const files = await fs.readdir(path);
 
-  const sortedFiles = files.sort((a, b) => a.localeCompare(b));
+  const sortedFiles = files.sort((a, b) => {
+    const cleanA = a.replace(/^\./, "");
+    const cleanB = b.replace(/^\./, "");
+    return cleanA.localeCompare(cleanB, undefined, { sensitivity: "base" });
+  });
 
   const options = program.opts();
 
   let renderingFiles = [];
 
   if (options.showHidden) {
-    renderingFiles = sortedFiles;
+    renderingFiles = [".", "..", ...sortedFiles];
   } else {
     renderingFiles = sortedFiles.filter((file) => !/^\./.test(file));
   }
 
   if (options.oneFile) {
     for (let file of renderingFiles) {
-      console.log(file);
+      console.log(await formatFileName(path, file));
     }
   } else {
-    console.log(renderingFiles.join(" "));
+    const formatted = await Promise.all(
+      renderingFiles.map((fileName) => {
+        return formatFileName(path, fileName);
+      }),
+    );
+    console.log(formatted.join(" "));
   }
 } catch (error) {
-  const state = await fs.stat(path).catch(() => null);
-  if (state && state.isFile()) {
+  const stats = await fs.stat(path).catch(() => null);
+  if (stats && stats.isFile()) {
     console.log(path);
   } else {
     console.log(
