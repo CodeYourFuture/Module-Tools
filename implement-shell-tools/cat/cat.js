c2d2692051f0db86db1de062c209a58384a22012
Craig D'Silva <craig.s.dsilva@gmail.com>
2026-03-25 11:11:29 +0000

    Add logic to count lines

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -1,7 +1,13 @@
+import { program } from "commander";
 import process from "node:process";
 import { promises as fs } from "node:fs";
 
-const argv = process.argv.slice(2);
+let showNumber = false;
+
+program.name("cat").description("Prints the output of a file to the console").option("-n, --number", "Displays the lines along with their number").argument("<path>", "The file path").allowExcessArguments()
+program.parse()
+
+const argv = program.args
 if (argv.length < 1) {
     console.error(`Expected exactly 1 or more arguments (paths) to be passed but got ${argv.length}.`);
     process.exit(1);
@@ -12,6 +18,9 @@ for (const path of argv) {
     stringArr.push(await fs.readFile(path, "utf-8"));
 }
 
-for (let i = 0; i < stringArr.length; i++) {
-    console.log(stringArr[i].trim())
+const flatArr = stringArr.flatMap(l => l.split("\n").map((l, i, a) => i < a.length - 1 ? l + "\n" : l).filter(l => l !== ""));
+
+if (!showNumber) console.log(flatArr.join(""))
+else {
+console.log(flatArr.map((l, i) => `${i + 1} ${l}`).join(""))
 }
