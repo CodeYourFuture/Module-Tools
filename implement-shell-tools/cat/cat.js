ed6323db872a93f48350f0279c8e68ffca6d2ba6
Craig D'Silva <craig.s.dsilva@gmail.com>
2026-03-25 10:25:41 +0000

    Fix newline

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -6,8 +6,12 @@ if (argv.length < 1) {
     console.error(`Expected exactly 1 or more arguments (paths) to be passed but got ${argv.length}.`);
     process.exit(1);
 }
-const paths = argv;
 
-for (const path of paths) {
-    console.log(await fs.readFile(path, "utf-8"));
+const stringArr = [];
+for (const path of argv) {
+    stringArr.push(await fs.readFile(path, "utf-8"));
+}
+
+for (let i = 0; i < stringArr.length; i++) {
+    console.log(stringArr[i].trim())
 }
