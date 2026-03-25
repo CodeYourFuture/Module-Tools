62821e43416a34326d047e1d412d5cc7a2d79f05
Craig D'Silva <craig.s.dsilva@gmail.com>
2026-03-25 18:58:06 +0000

    flags

--- implement-shell-tools/wc/wc.js
+++ implement-shell-tools/wc/wc.js
@@ -2,6 +2,10 @@ import { program } from "commander";
 import process from "node:process";
 import { promises as fs } from "node:fs";
 
+let showLineCount = true;
+let showWordCount = true;
+let showByteCount = true;
+
 program.name("wc").description("Print the number of lines, word and bytes for each file and a total if there are multiple files")
 .option("-l, --lines", "Print the number of lines")
 .option("-w, --words", "Print the number of words")
@@ -11,6 +15,11 @@ program.parse()
 
 const argv = program.args;
 
+if (Object.keys(program.opts()).length >= 1) {
+    showLineCount = program.opts().lines;
+    showWordCount = program.opts().words;
+    showByteCount = program.opts().bytes;
+}
 
 const stringArr = [];
 for (const path of argv) {
@@ -27,12 +36,18 @@ for (let i = 0; i < stringArr.length; i++) {
     const line = stringArr[i].split("\n").length
     const wc = stringArr[i].split(" ").flatMap(l => l.split("\n").map((l, i, a) => i < a.length - 1 ? l + "\n" : l).filter(l => l.trim() !== "")).length
     const lineByte = new Blob([stringArr[i]]).size
+    if (showLineCount) {
     arr.push(line - 1)
     lines += line - 1;
+    }
+    if (showWordCount) {
     arr.push(wc);
     words += wc;
+    }
+    if (showByteCount) {
     arr.push(lineByte)
     bytes += lineByte
+    }
     arr.push(argv[i])
     infoArr.push(arr.join(" "))
 }
