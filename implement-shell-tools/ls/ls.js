2bf3b2abfc5acfa6f81b9411980b02e5a2934529
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-03-25 14:07:58 +0000

    ls done - "-1 for all files including hiddens and -a does not show hidden files

---
+++ implement-shell-tools/ls/ls.js
@@ -0,0 +1,29 @@
+import { promises as fs } from "node:fs";
+
+//getting all commands 
+const args = process.argv.slice(2);
+
+const showOnePerLine = args.includes("-1"); 
+const showAllFilesWithoutHidden = args.includes("-a");
+
+//current path
+const path = args.find(arg => !arg.startsWith("-")) || ".";
+// if we do console.log("path=> ",path," args=> " ,args); it will give us this =>: path=>  sample-files  args=>  [ '-1', '-a', 'sample-files' ]
+
+const direc = await fs.readdir(path)
+// if the path is <sample-files> console.log(direc) gives us =>: [ '.hidden.txt', '1.txt', '2.txt', '3.txt', 'dir' ]
+
+if(showOnePerLine && showAllFilesWithoutHidden){
+    const visibleFiles = direc.filter(element => !element.startsWith(".")); // filtering hidden files which starts with "."
+    visibleFiles.forEach(element => {
+        console.log(element)
+    });
+}
+else if(showOnePerLine ){  // shows all file including .hidden files
+    direc.forEach(element => {
+        console.log(element)
+    })
+}
+
+
+
