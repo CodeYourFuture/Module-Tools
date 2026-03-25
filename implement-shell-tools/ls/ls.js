74981d40b6d33e219b92ba2c029cc65c6c1b1788
Craig D'Silva <craig.s.dsilva@gmail.com>
2026-03-25 13:03:18 +0000

    Add vertical print

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -1,10 +1,25 @@
+import { program } from "commander";
 import process from 'node:process';
 import fs from 'node:fs';
 
-const argv = process.argv.slice(2)
+let vertical = false;
+
+program.name("list-directory-contents").description("Shows all files and folders in a directory").option("-1", "List one file/directory per line").argument("[path]", "Path of the directory to list (defaults to .)")
+
+program.parse();
+
+const argv = program.args;
+
+vertical = program.opts()[1]
 
 const folderPath = argv[0] || ".";
 
 const contents = fs.readdirSync(folderPath);
 
-console.log(contents.join("  "));
+if (!vertical) {
+    console.log(contents.join("  "));
+} else {
+    for (const content of contents) {
+        console.log(content);
+    }
+}
