0edb723af2dfe51b33d14373b189653bb57ad4ff
Ahmad Hmedan <ahmadhmedann@gmail.com>
2026-03-28 21:27:31 +0000

    basic cat implemention

--- implement-shell-tools/cat/cat.mjs
+++ implement-shell-tools/cat/cat.mjs
@@ -1,18 +1,25 @@
 import { program } from "commander";
+import { error } from "node:console";
 import { promises as fs } from "node:fs";
 
 program
-  .name("implement my own version of `cat`.")
-  .description("I do not know what I'm doing")
+  .name("cat")
+  .description("cat sample-files/1.txt ")
   .argument("<path>", "The file path to process");
 program.parse();
 
 const argv = program.args;
 if (argv.length != 1) {
-console.error(`Expected exactly 1 argument (a path) to  be passed but got ${argv.length}`);
-process.exit(1);
+  console.error(
+    `Expected exactly 1 argument (a path) to  be passed but got ${argv.length}`,
+  );
+  process.exit(1);
 }
 const path = argv[0];
-
-const content = await fs.readFile(path, "utf-8");
-console.log(content);
+try {
+  const content = await fs.readFile(path, "utf-8");
+  process.stdout.write(content);
+} catch (error) {
+  console.error(error.message);
+  process.exit(1);
+}
