94a9be835a7ba22181323545c8d93b8075422499
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-03-30 20:51:46 +0100

    fixed ls

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -5,13 +5,15 @@ program
   .name("list files and directories")
   .option("-a", "show hidden files")
   .option("-1", "force displaying each item in a new line")
-  .argument("<paths...>", "path of directory");
+  .argument("[paths...]", "path of directory");
 
 program.parse();
 
 const showHiddenFiles = program.opts()["a"];
 const showFilesInLines = program.opts()["1"];
-const fetchedDirectories = await fetchDirectoriesFunc(program.args);
+const paths = program.args.length ? program.args : ["."];
+
+const fetchedDirectories = await fetchDirectoriesFunc(paths);
 
 console.log(formatDisplay(fetchedDirectories));
 
@@ -35,9 +37,27 @@ async function fetchDirectoriesFunc(directories) {
   for (const folderName of directories) {
     let files = await fs.readdir(folderName);
 
-    if (showHiddenFiles) files.unshift(".", "..");
+    // sort by name + but those starting with . at the end
+    files.sort((a, b) => {
+      const isHiddenA = a.startsWith(".");
+      const isHiddenB = b.startsWith(".");
 
-    files.sort((a, b) => a.localeCompare(b));
+      if (isHiddenA !== isHiddenB) {
+        return isHiddenA ? 1 : -1;
+      }
+
+      const cleanA = a.replace(/^\.+/, "");
+      const cleanB = b.replace(/^\.+/, "");
+
+      return cleanA.localeCompare(cleanB);
+    });
+
+    if (showHiddenFiles) {
+      files.unshift(".", "..");
+    } else {
+      files = files.filter((fileName) => !fileName.startsWith("."));
+    }
+
     result.push({
       folderName,
       files,
