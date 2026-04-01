129b9d7e95ef65855c5257f37fa4c459d8c27cae
TzeMingHo <tzemingho@gmail.com>
2026-04-01 14:08:56 +0100

    finished custom ls logic but it can be improved

--- implement-shell-tools/ls/custom_ls.py
+++ implement-shell-tools/ls/custom_ls.py
@@ -2,20 +2,40 @@ import argparse
 import os
 import re
 
+def sort_list(file_list):
+    return sorted(file_list, key=lambda file_name: re.sub(r"^\.", "", file_name).lower())
+
+def format_list(file_list):
+    return [f"\033[1;34m{file}\033[0m" if os.path.isdir(os.path.join(args.path, file)) else file for file in file_list]
+
 parser = argparse.ArgumentParser(
     prog="custom ls in python",
     description="trying to match behaviour as the actual ls"
 )
 
-parser.add_argument("-a", "--show_hidden", help="show hidden files")
+parser.add_argument("-a", "--show_hidden", action="store_true", help="show hidden files")
+parser.add_argument("-1", "--one_item", action="store_true", help="show one item per one line")
 parser.add_argument("path", help="directory path to process", nargs="?", default=".")
 
 args = parser.parse_args()
 
 files_array = os.listdir(args.path)
 
+sorted_files_array = sort_list(files_array)
+
 if (not args.show_hidden):
-    filtered_list = [file for file in files_array if re.match(r"^(?!\.)", file)]
-    sorted_file_list = sorted(filtered_list, key=lambda file_name: file_name.lower())
-    formatted_list = [f"\033[1;34m{file}\033[0m" if os.path.isdir(os.path.join(args.path, file)) else file for file in sorted_file_list]
-    print("  ".join(formatted_list))
+    filtered_list = [file for file in sorted_files_array if re.match(r"^(?!\.)", file)]
+    formatted_list = format_list(filtered_list)
+    if (args.one_item):
+        for item in formatted_list:
+            print(item)
+    else:
+        print("  ".join(formatted_list))
+else: 
+    sorted_hidden_list = [".", "..", *sorted_files_array]
+    formatted_hidden_list = format_list(sorted_hidden_list)
+    if (args.one_item):
+        for item in formatted_hidden_list:
+            print(item)
+    else:
+        print("  ".join(formatted_hidden_list))
