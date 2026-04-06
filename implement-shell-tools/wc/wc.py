57914a3351c7ba5973b14b8b39ee249bc0fff7a9
Ahmad Hmedan <ahmadhmedann@gmail.com>
2026-04-06 23:35:14 +0100

    add multiple files without totatl

--- implement-shell-tools/wc/wc.py
+++ implement-shell-tools/wc/wc.py
@@ -5,26 +5,30 @@ parser = argparse.ArgumentParser(
     description="wc implementation",
 )
 
-parser.add_argument("path", help="The file to process")
+parser.add_argument("paths", nargs="+", help="The file to process")
 parser.add_argument("-l", action="store_true", help="count lines")
 parser.add_argument("-w", action="store_true", help="count words")
 parser.add_argument("-c", action="store_true", help="count characters")
+args = parser.parse_args()
 
+for path in args.paths:
 
-args =parser.parse_args()
-with open(args.path,"r") as f:
-    content = f.read()
+    with open(path, "r") as f:
+        content = f.read()
 
-line_counter = len(content.split("\n"))-1
-words_counter = len(content.split())
-characters_counter = len(content)
-if(not args.l and not args.w and not args.c):
- print(f"{line_counter}  {words_counter}  {characters_counter} {args.path}")
+    line_counter = len(content.split("\n")) - 1
+    words_counter = len(content.split())
+    characters_counter = len(content)
+    if not args.l and not args.w and not args.c:
+        print(f"{line_counter}  {words_counter}  {characters_counter} {args.path}")
 
-else:
- results = []
- if(args.l):results.append(line_counter)
- if(args.w):results.append(words_counter)
- if(args.c):results.append(characters_counter)
- results=" ".join(map(str,results))
- print(results,args.path)
+    else:
+        results = []
+        if args.l:
+            results.append(line_counter)
+        if args.w:
+            results.append(words_counter)
+        if args.c:
+            results.append(characters_counter)
+        results = " ".join(map(str, results))
+        print(results, path)
