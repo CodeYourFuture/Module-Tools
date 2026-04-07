3a816e4d9e8805729ca46a41d1b13f26745d3f91
Ahmad Hmedan <ahmadhmedann@gmail.com>
2026-04-07 15:01:28 +0100

    clean the code

--- implement-shell-tools/ls/ls.py
+++ implement-shell-tools/ls/ls.py
@@ -7,27 +7,22 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument("path", nargs="?", help="The file to process", default=".")
-parser.add_argument("-1", "--one_per_line",action="store_true", help="one file per line")
+parser.add_argument(
+    "-1", "--one_per_line", action="store_true", help="one file per line"
+)
 parser.add_argument("-a", action="store_true", help="Show hidden files")
 
 
 args = parser.parse_args()
 
-path = Path(args.path)  # path is an object represent a location on disk
-   
-# else:
-#  for item in path.iterdir():  # iterdir is a method reads directory  contents
-#     if args.a:
-#         print(item.name)
-#     else:
-#         if not item.name.startswith("."):
-#             print(item.name)  # each item is a Path object
-items =[]
+path = Path(args.path)
+
+items = []
 for item in sorted(path.iterdir()):
     if args.a or not item.name.startswith("."):
         items.append(item.name)
 
-if(args.one_per_line):
+if args.one_per_line:
     for name in items:
         print(name)
 else:
