e31edff0a15020ce30369abf71b8b3f44b23cfca
Craig D'Silva <craig.dsilva10@gmail.com>
2026-04-08 16:21:09 +0100

    cat - complete

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser(prog="cat", description="Prints the output of a file to the console")
 parser.add_argument("-n", "--number", help="Displays the lines along with their number", action="store_true")
-parser.add_argument("-b", "--nonblank", help="Displays the lines along with their number", action="store_true")
+parser.add_argument("-b", "--nonblank", help="Displays the lines along with their number skipping the blank lines", action="store_true")
 parser.add_argument("path", help="The file path", nargs="+")
 
 args = parser.parse_args()
