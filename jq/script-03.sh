2c5d5290aebedb4675cc933078c1b6abf704970d
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-05-12 22:45:42 +0100

    update

--- jq/script-03.sh
+++ jq/script-03.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 # The input for this script is the person.json file.
 # TODO: Write a command to output the name of the person, then a comma, then their profession.
 # Your output should be exactly the string "Selma, Software Engineer", but should not contain any quote characters.
-jq -r '[.name, .profession] | join(", ")' person.json # join works on arrays thats why we need to to put values of name and profession in a array to be able to use join
+
+# Join works on arrays thats why we need to to put values of name and profession in a array to be able to use join
+jq -r '[.name, .profession] | join(", ")' person.json 
