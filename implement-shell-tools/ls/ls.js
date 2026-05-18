c0a4dca87a108f19350c51456f2d285afa98463a
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-05-18 20:09:28 +0100

    Add -a flag to show hidden files

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -3,14 +3,27 @@ import { promises as fs } from "node:fs";
 
 const args = process.argv.slice(2);
 
+let showAll = false;
 let path = ".";
 
-if (args[1]) {
-  path = args[1];
+for (const arg of args) {
+
+  if (arg === "-a") {
+    showAll = true;
+  }
+
+  else if (arg !== "-1") {
+    path = arg;
+  }
 }
 
 const files = await fs.readdir(path);
 
 for (const file of files) {
+
+  if (!showAll && file.startsWith(".")) {
+    continue;
+  }
+
   console.log(file);
 }
