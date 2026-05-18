b766aad552b0d03894c43943b6b21ba56ca2bc8a
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-05-18 19:00:54 +0100

    Add basic cat to read and print file contents

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -1,14 +1,14 @@
 import process from "node:process";
 import { promises as fs } from "node:fs";
 
-const argv = process.argv.slice(2);
-if (argv.length != 1) {
-    console.error(`Expected exactly 1 argument (a path) to be passed but got ${argv.length}.`);
-    process.exit(1);
-}
-const path = argv[0];
-const content = await fs.readFile(path, "utf-8");
+const paths = process.argv.slice(2);
 
-console.log(content);
-const theNumberOfLines = content.split("\n").length;
-console.log(`The number of lines in the file is ${theNumberOfLines}.`);
+if (paths.length === 0) {
+  console.error("Expected at least one file path.");
+  process.exit(1);
+}
+
+for (const path of paths) {
+  const content = await fs.readFile(path, "utf-8");
+  console.log(content);
+}
