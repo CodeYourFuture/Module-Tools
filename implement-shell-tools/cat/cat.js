dba8fd3aada2320062f22761c5cfc37e6f4a1b64
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-05-18 19:10:33 +0100

    Add -n flag to number all lines

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -1,7 +1,19 @@
 import process from "node:process";
 import { promises as fs } from "node:fs";
 
-const paths = process.argv.slice(2);
+const args = process.argv.slice(2);
+
+let showNumbers = false;
+const paths = [];
+
+for (const arg of args) {
+  if (arg === "-n") {
+    showNumbers = true;
+  } else {
+    paths.push(arg);
+  }
+}
+
 
 if (paths.length === 0) {
   console.error("Expected at least one file path.");
@@ -10,5 +22,18 @@ if (paths.length === 0) {
 
 for (const path of paths) {
   const content = await fs.readFile(path, "utf-8");
-  console.log(content);
+
+  const lines = content.split("\n");
+
+  if (showNumbers) {
+    let i = 1;
+
+    for (const line of lines) {
+      console.log(`${i} ${line}`);
+      i++;
+    }
+
+  } else {
+    console.log(content);
+  }
 }
