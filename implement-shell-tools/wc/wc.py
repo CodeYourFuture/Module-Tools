f2ad18acb68bef7163c23360317814224412c487
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-05-19 23:32:59 +0100

    Add -l -w -c flags to wc

--- implement-shell-tools/wc/wc.py
+++ implement-shell-tools/wc/wc.py
@@ -2,12 +2,32 @@ import sys
 
 args = sys.argv[1:]
 
-for path in args:
+option = "all"
+paths = []
+
+for arg in args:
+    if arg == "-l":
+        option = "l"
+    elif arg == "-w":
+        option = "w"
+    elif arg == "-c":
+        option = "c"
+    else:
+        paths.append(arg)
+
+for path in paths:
     with open(path, "r") as file:
         content = file.read()
 
-    lines = content.split("\n")
-    words = content.split(" ")
-    chars = content
+    lines = len(content.split("\n"))
+    words = len(content.split(" "))
+    chars = len(content)
 
-    print(len(lines), len(words), len(chars), path)
+    if option == "l":
+        print(lines, path)
+    elif option == "w":
+        print(words, path)
+    elif option == "c":
+        print(chars, path)
+    else:
+        print(lines, words, chars, path)
