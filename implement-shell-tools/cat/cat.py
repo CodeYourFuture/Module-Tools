f4ce299b6fc70021d4cdf49454fe465e1e9949bb
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-05-19 23:05:24 +0100

    Add -n flag to number all lines

--- implement-shell-tools/cat/cat.py
+++ implement-shell-tools/cat/cat.py
@@ -2,8 +2,24 @@ import sys
 
 args = sys.argv[1:]
 
-path = args[0]
+show_numbers = False
+paths = []
 
-with open(path, "r") as file:
-    content = file.read()
-    print(content)
+# separate flags and paths
+for arg in args:
+    if arg == "-n":
+        show_numbers = True
+    else:
+        paths.append(arg)
+
+for path in paths:
+    with open(path, "r") as file:
+        lines = file.readlines()
+
+        if show_numbers:
+            i = 1
+            for line in lines:
+                print(f"{i} {line.rstrip()}")
+                i += 1
+        else:
+            print("".join(lines))
