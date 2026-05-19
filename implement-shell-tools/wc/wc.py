65425b49acd85f9e7cc80c005b4011dd55b6438b
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-05-19 23:30:56 +0100

    Support multiple files in wc

--- implement-shell-tools/wc/wc.py
+++ implement-shell-tools/wc/wc.py
@@ -1,13 +1,13 @@
 import sys
 
 args = sys.argv[1:]
-path = args[0]
 
-with open(path, "r") as file:
-    content = file.read()
+for path in args:
+    with open(path, "r") as file:
+        content = file.read()
 
-lines = content.split("\n")
-words = content.split(" ")
-chars = content
+    lines = content.split("\n")
+    words = content.split(" ")
+    chars = content
 
-print(len(lines), len(words), len(chars))
+    print(len(lines), len(words), len(chars), path)
